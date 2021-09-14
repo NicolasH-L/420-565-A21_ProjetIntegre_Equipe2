5d3e1076bfa7aae589d948c2e27ca7aaa8000ba4
Anas <sinho919@hotmail.com>
2021-09-14 19:01:49 -0400

    ajout dossier projet intelliJ

---
+++ Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/ProjetIntegreEquipe2Application.java
@@ -0,0 +1,13 @@
+package com.equipe2.projet_integre_equipe2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetIntegreEquipe2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetIntegreEquipe2Application.class, args);
+    }
+
+}
---
+++ Projet_Integre_Equipe2/src/test/java/com/equipe2/projet_integre_equipe2/ProjetIntegreEquipe2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.equipe2.projet_integre_equipe2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetIntegreEquipe2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
