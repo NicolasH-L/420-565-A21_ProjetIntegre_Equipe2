f1f91feb3c24eb93b8b694e8846f5de29a8c5ccc
NicolasH-L <nhonglao@gmail.com>
2021-09-17 10:03:52 -0400

    Creation d'un component Moniteur

---
+++ Projet_Integre_Equipe2/src/main/resources/projet_integre_equipe2/src/components/Moniteur/Moniteur.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Moniteur.module.css';
+
+const Moniteur = () => (
+  <div className={styles.Moniteur}>
+    Moniteur Component
+  </div>
+);
+
+Moniteur.propTypes = {};
+
+Moniteur.defaultProps = {};
+
+export default Moniteur;
