5e7d9a269bb9d8cb0a64d4ac00172f22af61cc43
IglooSyrup <darckuvina@gmail.com>
2021-09-17 11:32:45 -0400

    Ajout des placeholder

--- Projet_Integre_Equipe2/src/main/resources/projet_integre_equipe2/src/components/Moniteur/Moniteur.js
+++ Projet_Integre_Equipe2/src/main/resources/projet_integre_equipe2/src/components/Moniteur/Moniteur.js
@@ -8,29 +8,32 @@ const Moniteur = () => {
       Moniteur Component
       <h1>Formulaire</h1>
       {/* <form onSubmit={}> */}
-      <form class="col-md-6">
-        <div class="form-group">
-          <label>Nom:</label>
-          <input type="text" className="form-control" />
-        </div>
-        <div class="form-group">
-          <label>Prénom:</label>
-          <input type="text" className="form-control" />
-        </div>
-        <div class="form-group">
-          <label>Mot de Passe:</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
-        </div>
-        <div class="form-group">
-          <label>Nom de l'entreprise:</label>
-          <input type="text" className="form-control" />
-        </div>
-        <div class="form-group">
-          <label>Email:</label>
-          <input type="email" className="form-control" />
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
+      <div className="mx-auto">
+        <form className="col-md-6">
+          <div class="form-group">
+            <label>Nom:</label>
+            <input type="text" className="form-control" placeholder="Nom"/>
+          </div>
+          <div class="form-group">
+            <label>Prénom:</label>
+            <input type="text" className="form-control" placeholder="Prénom"/>
+          </div>
+          <div class="form-group">
+            <label>Mot de Passe:</label>
+            <input type="password" className="form-control" placeholder="Mot de Passe" />
+          </div>
+          <div class="form-group">
+            <label>Nom de l'entreprise:</label>
+            <input type="text" className="form-control" placeholder="Nom de l'entreprise"/>
+          </div>
+          <div class="form-group">
+            <label>Courriel:</label>
+            <input type="email" className="form-control" placeholder="Courriel"/>
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+
     </div>
   )
 }
