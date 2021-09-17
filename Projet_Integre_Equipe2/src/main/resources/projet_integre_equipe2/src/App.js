fcf2ef1164c569cc1287af7bde4aa945f4af1f41
NicolasH-L <nhonglao@gmail.com>
2021-09-17 12:40:55 -0400

    Formulaire d'inscription pour le moniteur

--- Projet_Integre_Equipe2/src/main/resources/projet_integre_equipe2/src/App.js
+++ Projet_Integre_Equipe2/src/main/resources/projet_integre_equipe2/src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
-<<<<<<< HEAD
-import Moniteur from './components/Moniteur/Moniteur.js'
-import StudentRegistration from './StudentRegistration.js'
-=======
-import Moniteur from './components/Moniteur/Moniteur';
+import MonitorRegistration from './components/MonitorRegistration';
+import Moniteur from './components/MonitorRegistration';
 import StudentRegistration from './components/StudentRegistration';
->>>>>>> c8493108a885ae63c7bbe272ae9f634244f8ce51
 
 function App() {
   return (
     <div class="container-fluid">
-      <StudentRegistration/>
-<<<<<<< HEAD
-      <Moniteur>
-      </Moniteur>
-=======
->>>>>>> c8493108a885ae63c7bbe272ae9f634244f8ce51
+      {/* <StudentRegistration/> */}
+      <MonitorRegistration/>
     </div>
   );
 }
--- Projet_Integre_Equipe2/src/main/resources/projet_integre_equipe2/src/components/MonitorRegistration.js
+++ Projet_Integre_Equipe2/src/main/resources/projet_integre_equipe2/src/components/MonitorRegistration.js
@@ -2,10 +2,47 @@ import React from 'react'
 import {useState} from 'react'
 
 const MonitorRegistration = () =>{
+    const [lastName, setLastName] = useState("")
+    const [firstName, setFirstName] = useState("")
+    const [password, setPassword] = useState("")
+    const [enterpriseName, setEnterpriseName] = useState("")
+    const [email, setEmail] = useState("")
+    
+    const onSubmit = (e) => {
+        e.preventDefault()
 
+        if (!lastName && !firstName && !password && !enterpriseName && !email){
+            alert("Veuillez remplir tous les champs!")
+            return
+        }
+    }
+    
     return (
         <>
-
+            <form onSubmit={onSubmit}>
+                <h1 className="text-center">Formulaire d'inscription</h1>
+                <div class="form-group">
+                    <label for="lastName">Nom: </label>
+                    <input type="text" class="form-control" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+                </div>
+                <div class="form-group">
+                    <label for="firstName">Prénom: </label>
+                    <input type="firstName" class="form-control" id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+                </div>
+                <div class="form-group">
+                    <label for="password">Mot de passe: </label>
+                    <input type="password" class="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+                <div class="form-group">
+                    <label for="enterpriseName">Nom de l'entreprise: </label>
+                    <input type="enterpriseName" class="form-control" id="enterpriseName" value={enterpriseName} onChange={(e) => setEnterpriseName(e.target.value)}/>
+                </div>
+                <div class="form-group">
+                    <label for="email">Courriel: </label>
+                    <input type="text" class="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
         </>
     )
 }
