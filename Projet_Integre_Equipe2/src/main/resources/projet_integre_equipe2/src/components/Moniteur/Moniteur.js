8e372c485f2aeaf898a5993de96bf19e8cc95576
IglooSyrup <darckuvina@gmail.com>
2021-09-17 12:04:16 -0400

    Avant merge

--- Projet_Integre_Equipe2/src/main/resources/projet_integre_equipe2/src/components/Moniteur/Moniteur.js
+++ Projet_Integre_Equipe2/src/main/resources/projet_integre_equipe2/src/components/Moniteur/Moniteur.js
@@ -5,34 +5,34 @@ import styles from './Moniteur.module.css';
 const Moniteur = () => {
   return (
     <div className={styles.Moniteur}>
-      Moniteur Component
+      {/* Moniteur Component
       <h1>Formulaire</h1>
-      {/* <form onSubmit={}> */}
+      <a href="#">Entreprise</a>
       <div className="mx-auto">
         <form className="col-md-6">
-          <div class="form-group">
+          <div className="form-group">
             <label>Nom:</label>
             <input type="text" className="form-control" placeholder="Nom"/>
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <label>Prénom:</label>
             <input type="text" className="form-control" placeholder="Prénom"/>
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <label>Mot de Passe:</label>
             <input type="password" className="form-control" placeholder="Mot de Passe" />
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <label>Nom de l'entreprise:</label>
             <input type="text" className="form-control" placeholder="Nom de l'entreprise"/>
           </div>
-          <div class="form-group">
+          <div className="form-group">
             <label>Courriel:</label>
             <input type="email" className="form-control" placeholder="Courriel"/>
           </div>
           <input type="submit" value="Submit" />
         </form>
-      </div>
+      </div> */}
 
     </div>
   )
