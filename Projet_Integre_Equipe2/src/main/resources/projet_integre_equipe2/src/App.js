06f95eaace4fc209ad361b1266250121f0a6d291
NicolasH-L <nhonglao@gmail.com>
2021-09-17 19:24:44 -0400

    Nettoyage de code non utilise

--- Projet_Integre_Equipe2/src/main/resources/projet_integre_equipe2/src/App.js
+++ Projet_Integre_Equipe2/src/main/resources/projet_integre_equipe2/src/App.js
@@ -6,17 +6,6 @@ import Footer from './components/Footer';
 
 function App() {
   
-  const addStudents = async (task) => {
-    const res = await fetch('http://localhost:5000/students',
-    {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(task)
-    })
-  }
-  
   return (
     <div className="container-fluid">
       <NavbarHead/>
