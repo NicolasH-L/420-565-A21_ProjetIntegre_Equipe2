c1c506f6cccb7c7750beb2f8411e6a1c5ffb8bb0
IglooSyrup <darckuvina@gmail.com>
2021-09-17 13:35:03 -0400

    merge

--- Projet_Integre_Equipe2/src/main/resources/projet_integre_equipe2/src/App.js
+++ Projet_Integre_Equipe2/src/main/resources/projet_integre_equipe2/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
 import MonitorRegistration from './components/MonitorRegistration';
 import NavbarHead from './components/NavbarHead';
-import StudentRegistration from './components/StudentRegistration';
 
 function App() {
   return (
-    <div class="container-fluid">
+    <div className="container-fluid">
       <NavbarHead/>
       <MonitorRegistration/>
     </div>
