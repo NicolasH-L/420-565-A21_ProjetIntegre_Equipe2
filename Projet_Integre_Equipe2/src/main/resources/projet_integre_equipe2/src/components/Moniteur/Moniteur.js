318eb16050bf5728e98cbaa360ecd25e32ab7a99
IglooSyrup <darckuvina@gmail.com>
2021-09-17 11:25:35 -0400

    Modification du formulaire d'inscription du Moniteur

--- Projet_Integre_Equipe2/src/main/resources/projet_integre_equipe2/src/components/Moniteur/Moniteur.js
+++ Projet_Integre_Equipe2/src/main/resources/projet_integre_equipe2/src/components/Moniteur/Moniteur.js
@@ -4,18 +4,34 @@ import styles from './Moniteur.module.css';
 
 const Moniteur = () => {
   return (
-  <div className={styles.Moniteur}>
-    Moniteur Component
-    <h1>Formulaire</h1>
-    {/* <form onSubmit={}> */}
-    <form>
-        <label>
-          Name:
-          {/* <input type="text" value={this.state.value} onChange={this.handleChange} /> */}
-        </label>
+    <div className={styles.Moniteur}>
+      Moniteur Component
+      <h1>Formulaire</h1>
+      {/* <form onSubmit={}> */}
+      <form class="col-md-6">
+        <div class="form-group">
+          <label>Nom:</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div class="form-group">
+          <label>Prénom:</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div class="form-group">
+          <label>Mot de Passe:</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+        </div>
+        <div class="form-group">
+          <label>Nom de l'entreprise:</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div class="form-group">
+          <label>Email:</label>
+          <input type="email" className="form-control" />
+        </div>
         <input type="submit" value="Submit" />
-    </form>
-  </div>
+      </form>
+    </div>
   )
 }
 
