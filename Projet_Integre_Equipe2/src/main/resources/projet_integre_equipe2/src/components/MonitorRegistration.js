b42dcbb761259e4891c8a82b96afbde9e817fc0b
IglooSyrup <darckuvina@gmail.com>
2021-09-18 00:52:23 -0400

    merge

--- Projet_Integre_Equipe2/src/main/resources/projet_integre_equipe2/src/components/MonitorRegistration.js
+++ Projet_Integre_Equipe2/src/main/resources/projet_integre_equipe2/src/components/MonitorRegistration.js
@@ -7,11 +7,14 @@ const MonitorRegistration = () => {
 
     const onSubmit = (e) => {
         e.preventDefault()
-        console.log(monitor)
         if (!monitor.lastName || !monitor.firstName || !monitor.password || !monitor.enterpriseName || !monitor.email) {
             alert("Veuillez remplir tous les champs!")
             return
         }
+        else {
+            console.log(monitor)
+        }
+
     }
 
     const [error, setError] = useState({
@@ -20,24 +23,23 @@ const MonitorRegistration = () => {
     });
 
     const validateInput = (e) => {
-        // let pattern = RegExp("[^a-zA-ZéÉèÈïÏêÊ-]");
         let pattern;
         let inputError;
-        
+
         // changer pattern 
         if (e.target.type == "email") {
             pattern = new RegExp(/^([a-zA-Z0-9]+[\._:$!%\-+]{0,1}([a-zA-Z0-9])+)+@(([a-zA-Z0-9])+[\.\-]{0,1}([a-zA-Z0-9])+)+\.[a-zA-Z0-9]{2,4}$/);
-            console.log("s "+ pattern)
-        }else if(e.target.id == "lastName" || e.target.id == "firstName"){
+            console.log("s " + pattern)
+        } else if (e.target.id == "lastName" || e.target.id == "firstName") {
             pattern = new RegExp(/^([a-zA-ZéÉèÈïÏêÊ])(([a-zA-ZéÉèÈïÏêÊ]*|\-)[a-zA-ZéÉèÈïÏêÊ])*[a-zA-ZéÉèÈïÏêÊ]*$/);
             console.log(pattern)
-        }else if(e.target.id == "enterpriseName"){
+        } else if (e.target.id == "enterpriseName") {
             pattern = new RegExp(/^[^ ]+([ ]{1}[^ ]+)+$/)
-        }else if(e.target.id == "password"){
+        } else if (e.target.id == "password") {
             pattern = new RegExp(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/)
         }
 
-        if(pattern === undefined) {
+        if (pattern === undefined) {
             return;
         }
 
@@ -63,31 +65,26 @@ const MonitorRegistration = () => {
                     <div className="form-group">
                         <label htmlFor="lastName">Nom: </label>
                         {error.lastName !== "" ? error.lastName : ""}
-                        {/* <input type="text" className="form-control" id="lastName" placeholder="Entrez votre nom de famille" onChange={(e) => setMonitor({ ...monitor, lastName: e.target.value })} /> */}
                         <input type="text" name="Nom" className="form-control text-center" id="lastName" placeholder="Entrez votre nom de famille" onChange={validateInput} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="firstName">Prénom: </label>
                         {error.firstName !== "" ? error.firstName : ""}
-                        {/* <input type="firstName" className="form-control" id="firstName" placeholder="Entrez votre prénom" onChange={(e) => setMonitor({ ...monitor, firstName: e.target.value })} /> */}
                         <input type="text" name="Prénom" className="form-control text-center" id="firstName" placeholder="Entrez votre prénom" onChange={validateInput} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Mot de passe: </label>
                         {error.password !== "" ? error.password : ""}
-                        {/* <input type="password" name="Mot de passe" className="form-control text-center" id="password" placeholder="Entrez votre mot de passe" onChange={(e) => setMonitor({ ...monitor, password: e.target.value })} /> */}
                         <input type="password" name="Mot de passe" className="form-control text-center" id="password" placeholder="Entrez votre mot de passe" onChange={validateInput} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="enterpriseName">Nom de l'entreprise: </label>
                         {error.enterpriseName !== "" ? error.enterpriseName : ""}
-                        {/* <input type="text" name="Nom de l'entreprise" className="form-control text-center" id="enterpriseName" placeholder="Entrez le nom de l'entreprise" onChange={(e) => setMonitor({ ...monitor, enterpriseName: e.target.value })} /> */}
                         <input type="text" name="Nom de l'entreprise" className="form-control text-center" id="enterpriseName" placeholder="Entrez le nom de l'entreprise" onChange={validateInput} />
                     </div>
                     <div className="form-group">
                         <label htmlFor="email">Courriel: </label>
                         {error.email !== "" ? error.email : ""}
-                        {/* <input type="email" name="Nom de courriel" className="form-control" id="email" placeholder="Entrez votre adresse courriel" onChange={(e) => setMonitor({ ...monitor, email: e.target.value })} /> */}
                         <input type="email" name="Nom de courriel" className="form-control text-center" id="email" placeholder="Entrez votre adresse courriel" onChange={validateInput} />
                     </div>
                     <button type="submit" className="btn btn-primary">Submit</button>
