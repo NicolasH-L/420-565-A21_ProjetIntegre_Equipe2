3d022020cc8912428439a09cedd94fcbee1c11f4
Anas <sinho919@hotmail.com>
2021-09-23 17:47:17 -0400

    ajout supervisor dans model

--- Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/model/Supervisor.java
+++ Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/model/Supervisor.java
@@ -1,2 +1,12 @@
-package com.equipe2.projet_integre_equipe2.model;public class Supervisor {
+package com.equipe2.projet_integre_equipe2.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+
+public class Supervisor extends User implements Serializable {
+
+    private String matricule;
 }
