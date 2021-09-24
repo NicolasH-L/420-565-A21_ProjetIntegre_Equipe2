87c093b0d9aba2027137501ac8ec7f45e4c809fd
IglooSyrup <darckuvina@gmail.com>
2021-09-24 10:30:16 -0400

    EQ2-2 - Ajustement du CSS MonitorRegistration et Registration

--- React/projet_integre_equipe2/src/components/MonitorRegistration.js
+++ React/projet_integre_equipe2/src/components/MonitorRegistration.js
@@ -1,22 +1,23 @@
 import _ from 'lodash';
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 // import MonitorLogin from './MonitorLogin';
 // to use router hook we will need to npm i hookrouter
-const MonitorRegistration = ({onAdd}) => {
-    const [monitor, setMonitor] = useState({lastName: "", firstName: "", password: "", enterpriseName: "", email: "" });
+const MonitorRegistration = ({ onAdd }) => {
+    const [monitor, setMonitor] = useState({ lastName: "", firstName: "", password: "", enterpriseName: "", email: "" });
     const [error, setError] = useState({ lastName: "", firstName: "", password: "", enterpriseName: "", email: "" })
 
     const onSubmit = (e) => {
         e.preventDefault()
         if (!_.isEmpty(error.lastName) || !_.isEmpty(error.firstName) || !_.isEmpty(error.password) || !_.isEmpty(error.enterpriseName) || !_.isEmpty(error.email) ||
-        _.isEmpty(monitor.firstName) || _.isEmpty(monitor.lastName) || _.isEmpty(monitor.password) || _.isEmpty(monitor.enterpriseName) || _.isEmpty(monitor.email)) {
+            _.isEmpty(monitor.firstName) || _.isEmpty(monitor.lastName) || _.isEmpty(monitor.password) || _.isEmpty(monitor.enterpriseName) || _.isEmpty(monitor.email)) {
             alert("Veuillez remplir tous les champs!")
             return
         } else {
             console.log(monitor)
         }
-        onAdd({monitor})
+        onAdd({ monitor })
     }
 
     // const routes = {
@@ -33,16 +34,16 @@ const MonitorRegistration = ({onAdd}) => {
 
         if (e.target.name === "email")
             pattern = new RegExp(patternEmail);
-        else if (e.target.name === "lastName" || e.target.name === "firstName") 
+        else if (e.target.name === "lastName" || e.target.name === "firstName")
             pattern = new RegExp(patternName);
-        else if (e.target.name === "enterpriseName") 
+        else if (e.target.name === "enterpriseName")
             pattern = new RegExp(patternEnterprise)
-        else if (e.target.name === "password") 
+        else if (e.target.name === "password")
             pattern = new RegExp(patternPassword)
 
         if (pattern === undefined)
             return;
-        
+
         if (!pattern.test(e.target.value) || e.target.value === "") {
             e.target.style.borderColor = "red";
             e.target.style.boxShadow = "0 1px 1px red inset, 0 0 8px red";
@@ -66,26 +67,28 @@ const MonitorRegistration = ({onAdd}) => {
                 <div className="form-group">
                     <label htmlFor="firstName" className="text-secondary"><i className="fas fa-user"></i> Prénom: </label>
                     {error.firstName !== "" ? error.firstName : ""}
-                    <input type="text" className="form-control text-center" id="firstNameMonitor" name="firstName" placeholder="Entrez votre prénom" onChange={validateInput} required/>
+                    <input type="text" className="form-control text-center" id="firstNameMonitor" name="firstName" placeholder="Entrez votre prénom" onChange={validateInput} required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="password" className="text-secondary"><i className="fas fa-lock"></i> Mot de passe: </label>
                     {error.password !== "" ? error.password : ""}
-                    <input type="password" className="form-control text-center" id="passwordMonitor" name="password" placeholder="Entrez votre mot de passe" onChange={validateInput} required/>
+                    <input type="password" className="form-control text-center" id="passwordMonitor" name="password" placeholder="Entrez votre mot de passe" onChange={validateInput} required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="enterpriseName" className="text-secondary"><i class="fas fa-building"></i> Nom de l'entreprise: </label>
                     {error.enterpriseName !== "" ? error.enterpriseName : ""}
-                    <input type="text" className="form-control text-center" id="enterpriseNameMonitor" name="enterpriseName" placeholder="Entrez le nom de l'entreprise" onChange={validateInput} required/>
+                    <input type="text" className="form-control text-center" id="enterpriseNameMonitor" name="enterpriseName" placeholder="Entrez le nom de l'entreprise" onChange={validateInput} required />
                 </div>
                 <div className="form-group">
                     <label htmlFor="email" className="text-secondary"><i class="fas fa-at"></i> Courriel: </label>
                     {error.email !== "" ? error.email : ""}
-                    <input type="email" className="form-control text-center" id="emailMonitor" name="email" placeholder="Entrez votre adresse courriel" onChange={validateInput} required/>
+                    <input type="email" className="form-control text-center" id="emailMonitor" name="email" placeholder="Entrez votre adresse courriel" onChange={validateInput} required />
                 </div>
-                <div className="d-flex justify-content-end">
-                    <button type="submit" className="btn grad text-white mr-2">Soumettre</button>
-                    {/* <a className="btn grad text-white" href="/monitorLogin">Se connecter</a> */}
+                <div className="d-flex justify-content-center mt-4">
+                    <button type="submit" className="btn btn-block grad text-white">Soumettre</button>
+                </div>
+                <div className="d-flex justify-content-center mt-3">
+                    <Link className="btn btn-block grad text-white" to='/Login'>Se connecter</Link>
                 </div>
             </form>
         </div>
