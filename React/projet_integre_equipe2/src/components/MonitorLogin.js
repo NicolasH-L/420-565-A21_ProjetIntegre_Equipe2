e04119b77ef24a0f8cdce6dee4a081f36fe1b7cd
NicolasH-L <nhonglao@gmail.com>
2021-09-28 15:35:53 -0400

    EQ2-12 - Modification et mise a jour du formulaire de login pour le moniteur

--- React/projet_integre_equipe2/src/components/MonitorLogin.js
+++ React/projet_integre_equipe2/src/components/MonitorLogin.js
@@ -4,12 +4,12 @@ import { useState } from 'react'
 
 const MonitorLogin = ({ login }) => {
     const [monitor, setMonitor] = useState({password: "", email: "" });
-    const [error, setError] = useState({password: "", email: "" })
+    const [error, setError] = useState({credentials: ""})
 
     const onSubmit = (e) => {
         e.preventDefault()
-        if (!_.isEmpty(error.password) || !_.isEmpty(error.email) || _.isEmpty(monitor.password) || _.isEmpty(monitor.email)) {
-            alert("Veuillez remplir tous les champs!")
+        if (!_.isEmpty(error.credentials) || _.isEmpty(monitor.password) || _.isEmpty(monitor.email)) {
+            setError({...error, credentials: "Email ou mot de passe incorrect"})
             return
         } else {
             console.log(monitor)
@@ -21,14 +21,13 @@ const MonitorLogin = ({ login }) => {
         <div>
             <form className="container-fluid" onSubmit={onSubmit}>
                 <div className="form-group">
+                    {error.credentials !== "" ? error.credentials : ""}
                     <label htmlFor="emailMonitor" className="text-secondary"><i class="fas fa-at"></i> Courriel: </label>
-                    {error.email !== "" ? error.email : ""}
-                    <input type="email" className="form-control text-center" id="emailMonitor" name="email" placeholder="Entrez votre adresse courriel" onChange={validateInput} required/>
+                    <input type="email" className="form-control text-center" id="emailMonitor" name="email" placeholder="Entrez votre adresse courriel" onChange={(e) => setMonitor({...monitor, email: e.target.value})} required/>
                 </div>
                 <div className="form-group">
                     <label htmlFor="passwordMonitor" className="text-secondary"><i className="fas fa-lock"></i> Mot de passe: </label>
-                    {error.password !== "" ? error.password : ""}
-                    <input type="password" className="form-control text-center" id="passwordMonitor" name="password" placeholder="Entrez votre mot de passe" onChange={validateInput} required/>
+                    <input type="password" className="form-control text-center" id="passwordMonitor" name="password" placeholder="Entrez votre mot de passe" onChange={(e) => setMonitor({...monitor, password: e.target.value})} required/>
                 </div>
                 <div className="d-flex justify-content-end">
                     <button type="submit" className="btn btn-block grad text-white ">Connexion</button>
