3334de404698982ac432c515177b80b379ea1460
Jonathan-Lorfils <jonathan.11911@gmail.com>
2021-09-29 19:42:22 -0400

    EQ2-5 Creation du controller admin, de la methode login et ses tests

--- Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/controller/AdminController.java
+++ Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/controller/AdminController.java
@@ -1,22 +1,26 @@
 package com.equipe2.projet_integre_equipe2.controller;
 
+import com.equipe2.projet_integre_equipe2.model.Admin;
 import com.equipe2.projet_integre_equipe2.repository.AdminRepository;
 import com.equipe2.projet_integre_equipe2.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
-@RequestMapping("/monitors")
 public class AdminController {
 
     @Autowired
-    private AdminRepository adminRepository;
-
-    @Autowired
     private AdminService adminService;
+
+    @GetMapping("/admin/{username}/{password}")
+    public ResponseEntity<Admin> loginAdmin(@PathVariable String username, @PathVariable String password){
+        return adminService.login(username,password)
+                .map(admin1 -> ResponseEntity.status(HttpStatus.OK).body(admin1))
+                .orElse(ResponseEntity.status(HttpStatus.CONFLICT).body(new Admin()));
+    }
 
 
 }
