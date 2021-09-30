eafcee58a17b1825b47d2f6c4300758a955a9fe3
Kevin Blaires <kablaires@gmail.com>
2021-09-30 18:43:48 -0400

    EQ2-8 - Backend: Ajout du OfferRepository

---
+++ Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/repository/OfferRepository.java
@@ -0,0 +1,10 @@
+package com.equipe2.projet_integre_equipe2.repository;
+
+import com.equipe2.projet_integre_equipe2.model.Offer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OfferRepository extends JpaRepository<Offer, Integer> {
+    
+}
