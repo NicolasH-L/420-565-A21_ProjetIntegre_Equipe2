a30c502dc73fb7d4298bacf4e531e1fc5fa67e78
IglooSyrup <darckuvina@gmail.com>
2021-10-06 21:30:52 -0400

    EQ2-3 - Ajout des dates dans le formulaire

--- React/projet_integre_equipe2/src/components/MonitorInternshipOffer.js
+++ React/projet_integre_equipe2/src/components/MonitorInternshipOffer.js
@@ -9,9 +9,15 @@ import MonitorNavbar from './MonitorNavbar';
 const MonitorInternshipOffer = ({ onAdd }) => {
     const history = useHistory()
     const historyState = useHistory().location.state;
-    var emailMonitor
-    var company
-    if (historyState != undefined){
+    const timeElapsed = Date.now()
+    const today = new Date(timeElapsed).toISOString().split('T')[0]
+    let futureDate = new Date(timeElapsed)
+    futureDate.setDate(futureDate.getDate() + 220)
+    let futureDateFormat = futureDate.toISOString().split('T')[0]
+
+    let emailMonitor
+    let company
+    if (historyState != undefined) {
         emailMonitor = historyState.email
         company = historyState.companyName
     }
@@ -19,22 +25,28 @@ const MonitorInternshipOffer = ({ onAdd }) => {
     const [offer, setOffer] = useState({
         companyName: company, address: "", salary: "",
         jobTitle: "", description: "", skills: "",
-        jobSchedules: "", workingHours: "", monitorEmail: emailMonitor
+        jobSchedules: "", workingHours: "", monitorEmail: emailMonitor,
+        displayDate: "", deadlineDate: "", startInternshipDate: "", endInternshipDate: ""
     })
     const [error, setError] = useState({
-        address: "", salary: "",
-        jobTitle: "", description: "", skills: "",
-        jobSchedules: "", workingHours: "", monitorEmail: ""
+        address: "", salary: "", jobTitle: "", description: "",
+        skills: "", jobSchedules: "", workingHours: "",
+        displayDate: "", deadlineDate: "", startInternshipDate: "", endInternshipDate: ""
     })
     const onSubmit = (e) => {
         e.preventDefault()
 
-        if (!_.isEmpty(error.companyName) || !_.isEmpty(error.address) || !_.isEmpty(error.salary) ||
-            !_.isEmpty(error.jobTitle) || !_.isEmpty(error.description) || !_.isEmpty(error.skills) ||
-            !_.isEmpty(error.jobSchedules) || !_.isEmpty(error.workingHours) || !_.isEmpty(error.monitorEmail) ||
+        if (!_.isEmpty(error.address) || !_.isEmpty(error.salary) || !_.isEmpty(error.jobTitle) ||
+            !_.isEmpty(error.description) || !_.isEmpty(error.skills) || !_.isEmpty(error.jobSchedules) ||
+            !_.isEmpty(error.workingHours) || !_.isEmpty(error.displayDate) || !_.isEmpty(error.deadlineDate) ||
+            !_.isEmpty(error.startInternshipDate) || !_.isEmpty(error.endInternshipDate) ||
             _.isEmpty(offer.companyName) || _.isEmpty(offer.address) || _.isEmpty(offer.salary) ||
             _.isEmpty(offer.jobTitle) || _.isEmpty(offer.description) || _.isEmpty(offer.skills) ||
-            _.isEmpty(offer.jobSchedules) || _.isEmpty(offer.workingHours)) {
+            _.isEmpty(offer.jobSchedules) || _.isEmpty(offer.workingHours) || _.isEmpty(offer.monitorEmail) 
+            ||
+            _.isEmpty(offer.displayDate) || _.isEmpty(offer.deadlineDate) || _.isEmpty(offer.startInternshipDate) ||
+            _.isEmpty(offer.endInternshipDate)
+            ) {
             alert("Veuillez remplir tous les champs!")
             return
         } else {
@@ -49,7 +61,7 @@ const MonitorInternshipOffer = ({ onAdd }) => {
         }
     }
 
-    function submitOfferSuccess(){
+    function submitOfferSuccess() {
         alert("Ajout de l'offre de stage avec succès")
         document.getElementById("monitorInternshipForm").reset()
         history.push("/Monitor", history)
@@ -86,11 +98,7 @@ const MonitorInternshipOffer = ({ onAdd }) => {
         else if (e.target.name === "jobSchedules")
             console.log("pattern : " + pattern)
 
-        if (pattern === undefined && e.target.name === "jobSchedule" && e.target.value === "") {
-            e.target.style.borderColor = "red"
-            e.target.style.boxShadow = "0 1px 1px red inset, 0 0 8px red"
-            inputError = <strong className="text-danger"> Erreur <i className="fas fa-exclamation-circle text-danger fa-sm" ></i></strong>
-        } else if (pattern !== undefined && !pattern.test(e.target.value) || e.target.value === "") {
+        if (pattern === undefined && e.target.name === "jobSchedules" && e.target.value === "DEFAULT" ||  pattern !== undefined && !pattern.test(e.target.value) || e.target.value === "") {
             e.target.style.borderColor = "red"
             e.target.style.boxShadow = "0 1px 1px red inset, 0 0 8px red"
             inputError = <strong className="text-danger"> Erreur <i className="fas fa-exclamation-circle text-danger fa-sm" ></i></strong>
@@ -132,12 +140,12 @@ const MonitorInternshipOffer = ({ onAdd }) => {
                         <div className="form-group">
                             <label htmlFor="description" className="text-secondary"><i className="fas fa-clipboard"></i> Description: </label>
                             {error.description !== "" ? error.description : ""}
-                            <textarea type="text" className="form-control text-center" id="description" name="description" rows="3" onChange={validateInput} required />
+                            <textarea type="text" className="form-control text-center" id="description" name="description" rows="3" placeholder="Entrez la description"  onChange={validateInput} required />
                         </div>
                         <div className="form-group">
                             <label htmlFor="skills" className="text-secondary"><i className="fas fa-book"></i> Compétences: </label>
                             {error.skills !== "" ? error.skills : ""}
-                            <textarea type="text" className="form-control text-center" id="skills" name="skills" rows="3" onChange={validateInput} required />
+                            <textarea type="text" className="form-control text-center" id="skills" name="skills" rows="3" placeholder="Entrez les compétences" onChange={validateInput} required />
                         </div>
                         <div className="form-group">
                             <label htmlFor="monitorEmail" className="text-secondary"><i className="fas fa-at"></i> Répresentant de l'entreprise (email): </label>
@@ -149,15 +157,35 @@ const MonitorInternshipOffer = ({ onAdd }) => {
                             <input type="text" className="form-control text-center" id="workingHours" name="workingHours" placeholder="Entrez le nombre d'heures de travail" onChange={validateInput} required />
                         </div>
                         <div className="form-group">
-                            <label htmlFor="jobType" className="text-secondary"><i className="fas fa-calendar-alt"></i> Horaire de travail: </label>
+                            <label htmlFor="jobSchedules" className="text-secondary"><i className="fas fa-calendar-alt"></i> Horaire de travail: </label>
                             {error.jobSchedules !== "" ? error.jobSchedules : ""}
-                            <select defaultValue={'DEFAULT'} className="form-control text-center" id="jobSchedules" name="jobSchedules" onChange={validateInput} required >
-                                <option value="DEFAULT" disabled>Veuillez choisir le type d'horaire</option>
+                            <select defaultValue={'DEFAULT'} className="form-control text-center" id="jobSchedules" name="jobSchedules" onChange={validateInput} required>
+                                <option value="DEFAULT">Veuillez choisir le type d'horaire</option>
                                 <option value="Temps plein">Temps plein</option>
                                 <option value="Temps partiel">Temps partiel</option>
                                 <option value="Jour">Jour</option>
                                 <option value="Soir">Soir</option>
                             </select>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="displayDate" className="text-secondary"><i className="fas fa-calendar"></i> Date d'affichage:</label>
+                            {error.displayDate !== "" ? error.displayDate : ""}
+                            <input type="date" min={today} max={futureDateFormat} id="displayDate" name="displayDate" className="form-control text-center" onChange={validateInput} required></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="deadlineDate" className="text-secondary"><i className="fas fa-calendar"></i> Date limite:</label>
+                            {error.deadlineDate !== "" ? error.deadlineDate : ""}
+                            <input type="date" min={today} max={futureDateFormat} id="deadlineDate" name="deadlineDate" className="form-control text-center" onChange={validateInput} required></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="startInternshipDate" className="text-secondary"><i className="fas fa-calendar"></i> Début stage</label>
+                            {error.startInternshipDate !== "" ? error.startInternshipDate : ""}
+                            <input type="date" min={today} max={futureDateFormat} id="startInternshipDate" name="startInternshipDate" className="form-control text-center" onChange={validateInput} required></input>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="endInternshipDate" className="text-secondary"><i className="fas fa-calendar"></i> Fin stage</label>
+                            {error.endInternshipDate !== "" ? error.endInternshipDate : ""}
+                            <input type="date" min={today} max={futureDateFormat} id="endInternshipDate" name="endInternshipDate" className="form-control text-center" onChange={validateInput} required></input>
                         </div>
                         <div className="d-flex justify-content-center">
                             <button type="submit" className="btn btn-block grad text-white">Soumettre</button>
