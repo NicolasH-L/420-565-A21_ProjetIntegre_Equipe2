0509e68682276cb2f3c033e066d292258f4db9a0
IglooSyrup <darckuvina@gmail.com>
2021-10-07 21:21:37 -0400

    EQ2-3 - Code review

--- React/projet_integre_equipe2/src/components/Monitor.js
+++ React/projet_integre_equipe2/src/components/Monitor.js
@@ -1,27 +1,12 @@
 import React from 'react'
-import { useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
 import MonitorNavbar from './MonitorNavbar'
 
 const Monitor = () => {
-    const history = useHistory()
-    const monitor = history.location.state
-    useEffect(() => {
-        console.log(monitor)
-        if (monitor !== undefined)
-            console.log(monitor)
-        else
-            history.push("/")
-    }, [])
-
-    if (history.location.state === undefined) {
-        return null
-    } else
-        return (
-            < div >
-                <MonitorNavbar/>
-            </div >
-        )
+    return (
+        < div >
+            <MonitorNavbar />
+        </div >
+    )
 }
 
 export default Monitor
