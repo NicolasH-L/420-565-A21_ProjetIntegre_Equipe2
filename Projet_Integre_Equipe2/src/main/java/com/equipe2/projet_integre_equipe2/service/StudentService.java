9d6865ca3c01dfecc8608bc4817bbc0ee069fcc6
IglooSyrup <darckuvina@gmail.com>
2021-10-10 19:17:17 -0400

    EQ2-20 - Ajout de la fonctionalité pour savoir si un étudiant a un cv valide

--- Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/service/StudentService.java
+++ Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/service/StudentService.java
@@ -1,6 +1,5 @@
 package com.equipe2.projet_integre_equipe2.service;
 
-import com.equipe2.projet_integre_equipe2.model.Offer;
 import com.equipe2.projet_integre_equipe2.model.Student;
 import com.equipe2.projet_integre_equipe2.repository.StudentRepository;
 import org.springframework.stereotype.Service;
@@ -13,11 +12,11 @@ public class StudentService {
 
     public StudentRepository studentRepository;
 
-    public StudentService(StudentRepository studentRepository){
+    public StudentService(StudentRepository studentRepository) {
         this.studentRepository = studentRepository;
     }
 
-    public Optional<Student> registerStudent(Student student){
+    public Optional<Student> registerStudent(Student student) {
         try {
             return Optional.of(studentRepository.save(student));
         } catch (Exception exception) {
@@ -25,7 +24,7 @@ public class StudentService {
         }
     }
 
-    public Optional<Student> loginStudent(String matricule, String password){
+    public Optional<Student> loginStudent(String matricule, String password) {
         try {
             return Optional.of(studentRepository.findByMatriculeAndPassword(matricule, password));
         } catch (Exception exception) {
@@ -33,7 +32,11 @@ public class StudentService {
         }
     }
 
-    public Optional<List<Student>> getAllStudents(){
+    public Optional<Boolean> isValidCvStudent(String matricule) {
+        return Optional.of(studentRepository.existsByMatriculeAndIsCvValidTrue(matricule));
+    }
+
+    public Optional<List<Student>> getAllStudents() {
         try {
             return Optional.of(studentRepository.findAll());
         } catch (Exception e) {
