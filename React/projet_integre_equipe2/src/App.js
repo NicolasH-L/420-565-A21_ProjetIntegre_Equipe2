1f93f637a61acfa725582c608c5d7f0b0f1093d3
IglooSyrup <darckuvina@gmail.com>
2021-10-10 16:35:43 -0400

    EQ2-20 - Ajout de la route vers StudentInternshipOffers

--- React/projet_integre_equipe2/src/App.js
+++ React/projet_integre_equipe2/src/App.js
@@ -14,6 +14,7 @@ import StudentUploadCV from './components/StudentUploadCV';
 import OfferView from './components/OfferView';
 import AdminStudentCvList from './components/AdminStudentCvList';
 import AdminStudentList from './components/AdminStudentList';
+import StudentInternshipOffers from './components/StudentInternshipOffers'
 
 function App() {
   return (
@@ -37,6 +38,8 @@ function App() {
           <Route path = "/Supervisor" component={Supervisor}/>
           <Route path = "/AdminStudentList" component={AdminStudentList}/>
           <Route path = "/AdminStudentCvList" component={AdminStudentCvList}/>
+          <Route path = "/StudentInternshipOffers" component={StudentInternshipOffers}/>
+
           <Footer/>
         </div>
       </Router>
--- React/projet_integre_equipe2/src/components/Student.js
+++ React/projet_integre_equipe2/src/components/Student.js
@@ -2,18 +2,19 @@ import React from 'react'
 import { useHistory } from 'react-router-dom'
 import StudentUploadCV from './StudentUploadCV'
 import { useEffect } from 'react'
+import StudentNavbar from './StudentNavbar'
 
 const Student = () => {
     const history = useHistory()
 
-    useEffect(() => {
-        history.push("/StudentUploadCV",history.location.state)
-    }, [])
+    // useEffect(() => {
+    //     history.push("/StudentUploadCV",history.location.state)
+    // }, [])
     
 
     return (
         <div>
-            
+            <StudentNavbar></StudentNavbar>
         </div>
     )
 }
