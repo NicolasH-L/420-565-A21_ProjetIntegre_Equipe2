94c2aa76d4f159cb88540eb22f6f69c06f80c921
IglooSyrup <darckuvina@gmail.com>
2021-10-11 01:27:25 -0400

    Front-end: enlever toutes les ; inutiles

--- React/projet_integre_equipe2/src/App.js
+++ React/projet_integre_equipe2/src/App.js
@@ -1,50 +1,50 @@
-import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Footer from './components/Footer';
-import Registration from './components/Registration/Registration';
-import Login from './components/Login/Login';
-import AdminInternshipOffer from './components/AdminInternshipOffer';
-import AdminInternshipOfferList from './components/AdminInternshipOfferList';
-import Admin from './components/Admin';
-import Monitor from './components/Monitor';
-import MonitorInternshipOffer from './components/MonitorInternshipOffer';
-import Student from './components/Student';
-import Supervisor from './components/Supervisor';
-import StudentUploadCV from './components/StudentUploadCV';
-import OfferView from './components/OfferView';
-import AdminStudentCvList from './components/AdminStudentCvList';
-import AdminStudentList from './components/AdminStudentList';
-import AdminViewStudentCV from './components/AdminViewStudentCV';
-import StudentInternshipOffers from './components/StudentInternshipOffers'
+import './App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Footer from './components/Footer'
+import Registration from './components/Registration/Registration'
+import Login from './components/Login/Login'
+import AdminInternshipOffer from './components/AdminInternshipOffer'
+import AdminInternshipOfferList from './components/AdminInternshipOfferList'
+import Admin from './components/Admin'
+import Monitor from './components/Monitor'
+import MonitorInternshipOffer from './components/MonitorInternshipOffer'
+import Student from './components/Student'
+import Supervisor from './components/Supervisor'
+import StudentUploadCV from './components/StudentUploadCV'
+import OfferView from './components/OfferView'
+import AdminStudentCvList from './components/AdminStudentCvList'
+import AdminStudentList from './components/AdminStudentList'
+import AdminViewStudentCV from './components/AdminViewStudentCV'
+import StudentInternshipListOffers from './components/StudentInternshipListOffers'
 
 function App() {
   return (
-      <Router>
-        <div>
-          <Route path = "/" exact render={(props) => (
+    <Router>
+      <div>
+        <Route path="/" exact render={(props) => (
           <>
-            <Login/>
+            <Login />
           </>
-        )}/>
-          <Route path = "/Login" component={Login}/>
-          <Route path = "/Registration" component={Registration}/>
-          <Route path = "/Admin" component={Admin}/>
-          <Route path = "/AdminOffer" component={AdminInternshipOffer}/>
-          <Route path = "/AdminOffersList" component={AdminInternshipOfferList}/>
-          <Route path = "/Monitor" component={Monitor}/>
-          <Route path = "/MonitorOffer" component={MonitorInternshipOffer}/>
-          <Route path = "/Student" component={Student}/>
-          <Route path = "/StudentUploadCV" component={StudentUploadCV}/>
-          <Route path = "/OfferView" component={OfferView}/>
-          <Route path = "/Supervisor" component={Supervisor}/>
-          <Route path = "/AdminStudentList" component={AdminStudentList}/>
-          <Route path = "/AdminStudentCvList" component={AdminStudentCvList}/>
-          <Route path = "/StudentInternshipOffers" component={StudentInternshipOffers}/>
-          <Route path = "/AdminViewStudentCV" component={AdminViewStudentCV}/>
-          <Footer/>
-        </div>
-      </Router>
-  );
+        )} />
+        <Route path="/Login" component={Login} />
+        <Route path="/Registration" component={Registration} />
+        <Route path="/Admin" component={Admin} />
+        <Route path="/AdminOffer" component={AdminInternshipOffer} />
+        <Route path="/AdminOffersList" component={AdminInternshipOfferList} />
+        <Route path="/Monitor" component={Monitor} />
+        <Route path="/MonitorOffer" component={MonitorInternshipOffer} />
+        <Route path="/Student" component={Student} />
+        <Route path="/StudentUploadCV" component={StudentUploadCV} />
+        <Route path="/OfferView" component={OfferView} />
+        <Route path="/Supervisor" component={Supervisor} />
+        <Route path="/AdminStudentList" component={AdminStudentList} />
+        <Route path="/AdminStudentCvList" component={AdminStudentCvList} />
+        <Route path="/StudentInternshipListOffers" component={StudentInternshipListOffers} />
+        <Route path="/AdminViewStudentCV" component={AdminViewStudentCV} />
+        <Footer />
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
