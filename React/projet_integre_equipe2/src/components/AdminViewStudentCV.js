1c07e0a82a909a6c99e06c3cbcdbbc946078b387
Jonathan-Lorfils <jonathan.11911@gmail.com>
2021-10-12 12:08:29 -0400

    ÈÃEQ2-114 Fix probleme history.state.location dans NavbarStudent

--- React/projet_integre_equipe2/src/components/AdminViewStudentCV.js
+++ React/projet_integre_equipe2/src/components/AdminViewStudentCV.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
+import { Document } from 'react-pdf'
+import { pdfjs } from 'react-pdf';
 
 const AdminViewStudentCV = () => {
+    pdfjs.GlobalWorkerOptions.workerSrc = 'pdf.worker.min.js';
     const history = useHistory()
-    const document = history.location.state
-    
+    const document = history.location.state.data
+
     return (
         <div>
-            
+            <Document
+                file={{
+
+                }}
+            ></Document>
         </div>
     )
 }
