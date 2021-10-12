472a51fe246023ed176417069927f4b0023ceaf1
Jonathan-Lorfils <jonathan.11911@gmail.com>
2021-10-12 13:11:03 -0400

    EQ2-114 Ajout de la fonctionnalite permettant d'afficher le cv (front-end)

--- React/projet_integre_equipe2/src/components/AdminViewStudentCV.js
+++ React/projet_integre_equipe2/src/components/AdminViewStudentCV.js
@@ -1,21 +1,58 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
-import { Document } from 'react-pdf'
-import { pdfjs } from 'react-pdf';
+import AdminNavbar from './AdminNavbar'
+import { Document, Page, pdfjs } from '../../node_modules/react-pdf/dist/esm/entry.webpack';
 
 const AdminViewStudentCV = () => {
-    pdfjs.GlobalWorkerOptions.workerSrc = 'pdf.worker.min.js';
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
     const history = useHistory()
-    const document = history.location.state.data
+    const document = history.location.state
+    var data = document.data
+
+    const options = {
+        cMapUrl: 'cmaps/',
+        cMapPacked: true,
+    };
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+        setPageNumber(1)
+    }
+
+    console.log(data)
+
+    function _base64ToArrayBuffer(base64) {
+        var binary_string = window.atob(base64);
+        var len = binary_string.length;
+        var bytes = new Uint8Array(len);
+        for (var i = 0; i < len; i++) {
+            bytes[i] = binary_string.charCodeAt(i);
+        }
+        return bytes.buffer;
+    }
 
     return (
         <div>
+            <AdminNavbar />
             <Document
-                file={{
-
-                }}
-            ></Document>
+            file={{data:_base64ToArrayBuffer(data)}}
+            onLoadSuccess={onDocumentLoadSuccess}
+            options={options}
+          >
+            {
+              Array.from(
+                new Array(numPages),
+                (el, index) => (
+                  <Page
+                    key={`page_${index + 1}`}
+                    pageNumber={index + 1}
+                  />
+                ),
+              )
+            }
+          </Document>
         </div>
     )
 }
