84386c8b0fb4fb02123334033b5b2dfc7f8a0596
Jonathan-Lorfils <jonathan.11911@gmail.com>
2021-10-14 20:17:31 -0400

    EQ2-114 Ajout de boutons pour changer de page dans le document

--- React/projet_integre_equipe2/src/components/AdminViewStudentCV.js
+++ React/projet_integre_equipe2/src/components/AdminViewStudentCV.js
@@ -5,56 +5,77 @@ import AdminNavbar from './AdminNavbar'
 import { Document, Page, pdfjs } from '../../node_modules/react-pdf/dist/esm/entry.webpack';
 
 const AdminViewStudentCV = () => {
-    const [numPages, setNumPages] = useState(null);
-    const [pageNumber, setPageNumber] = useState(1);
-    const history = useHistory()
-    const document = history.location.state
-    var data = document.data
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const history = useHistory()
+  const document = history.location.state
+  var data = document.data
 
-    const options = {
-        cMapUrl: 'cmaps/',
-        cMapPacked: true,
-    };
+  const options = {
+    cMapUrl: 'cmaps/',
+    cMapPacked: true,
+  };
 
-    function onDocumentLoadSuccess({ numPages }) {
-        setNumPages(numPages);
-        setPageNumber(1)
+  function changePage(offset) {
+    setPageNumber(prevPageNumber => prevPageNumber + offset);
+  }
+
+  function previousPage() {
+    changePage(-1);
+  }
+
+  function nextPage() {
+    changePage(1);
+  }
+
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
+
+  function _base64ToArrayBuffer(base64) {
+    var binary_string = window.atob(base64);
+    var len = binary_string.length;
+    var bytes = new Uint8Array(len);
+    for (var i = 0; i < len; i++) {
+      bytes[i] = binary_string.charCodeAt(i);
     }
+    return bytes.buffer;
+  }
 
-    console.log(data)
+  const content = _base64ToArrayBuffer(data)
 
-    function _base64ToArrayBuffer(base64) {
-        var binary_string = window.atob(base64);
-        var len = binary_string.length;
-        var bytes = new Uint8Array(len);
-        for (var i = 0; i < len; i++) {
-            bytes[i] = binary_string.charCodeAt(i);
-        }
-        return bytes.buffer;
-    }
-
-    return (
-        <div>
-            <AdminNavbar />
-            <Document
-            file={{data:_base64ToArrayBuffer(data)}}
-            onLoadSuccess={onDocumentLoadSuccess}
-            options={options}
-          >
-            {
-              Array.from(
-                new Array(numPages),
-                (el, index) => (
-                  <Page
-                    key={`page_${index + 1}`}
-                    pageNumber={index + 1}
-                  />
-                ),
-              )
-            }
-          </Document>
+  return (
+    <div>
+      <AdminNavbar />
+      <div className="justify-content-center d-flex"> 
+        <Document width="200em"
+          file={content}
+          onLoadSuccess={onDocumentLoadSuccess}
+          options={options}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+      </div>
+      <div className="justify-content-center d-flex">
+          <p>
+            Page {pageNumber || (numPages ? 1 : "--")} of {numPages || "--"}
+          </p>
         </div>
-    )
+      <div className="justify-content-center d-flex">
+          <button className="btn btn-primary mx-3" type="button" disabled={pageNumber <= 1} onClick={previousPage}>
+            Previous
+          </button>
+          <button
+            className="btn btn-primary"
+            type="button"
+            disabled={pageNumber >= numPages}
+            onClick={nextPage}
+          >
+            Next
+          </button>
+        </div>
+    </div>
+  )
 }
 
 export default AdminViewStudentCV
