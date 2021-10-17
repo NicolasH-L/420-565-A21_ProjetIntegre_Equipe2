2fdf1fd2ce68c35ce8333678d28f534efd76e005
Jonathan-Lorfils <jonathan.11911@gmail.com>
2021-10-17 18:34:18 -0400

    EQ2-30 Ajout de la variable currentStatus dans Student

--- Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/model/Student.java
+++ Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/model/Student.java
@@ -14,14 +14,17 @@ public class Student extends User implements Serializable {
     @Column(unique = true, length = 200)
     private String matricule;
     private Boolean isCvValid = false;
+    private String currentStatus = "Recherche";
+
 
     @Builder(builderMethodName = "studentBuilder")
-    public Student(Integer id, String firstName, String lastName, String password, String matricule, Boolean isCvValid) {
+    public Student(Integer id, String firstName, String lastName, String password, String matricule, Boolean isCvValid, String currentStatus) {
         super.setId(id);
         super.setLastName(lastName);
         super.setFirstName(firstName);
         super.setPassword(password);
         this.matricule = matricule;
         this.isCvValid = isCvValid;
+        this.currentStatus = currentStatus;
     }
 }
