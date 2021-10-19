c434078c249380551790c94006bccb7e4ba5a082
Kevin Blaires <kablaires@gmail.com>
2021-10-19 15:13:47 -0400

    EQ2-23 - Backend: Ajout de la class Internship

---
+++ Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/model/Internship.java
@@ -0,0 +1,33 @@
+package com.equipe2.projet_integre_equipe2.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Internship {
+
+    @Id
+    @GeneratedValue
+    private int idInternship;
+
+    private Boolean isSignedByStudent;
+    private Boolean isSignedByMonitor;
+
+    @OneToOne
+    private Offer offer;
+
+    @OneToOne
+    private Student student;
+}
