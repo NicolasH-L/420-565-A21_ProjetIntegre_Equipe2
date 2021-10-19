e7d74501ea57d7dd32561802ce2360d7d6bf6dbd
Anas <sinho919@hotmail.com>
2021-10-19 15:59:49 -0400

    EQ2-7 Backend: ajout methode pour get tous les StudentOffers à partir du IdOffer dans le repo et le service

--- Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/repository/StudentOfferRepository.java
+++ Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/repository/StudentOfferRepository.java
@@ -4,8 +4,11 @@ import com.equipe2.projet_integre_equipe2.model.StudentOffer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentOfferRepository extends JpaRepository<StudentOffer, Integer> {
 
     boolean existsStudentOfferByOffer_IdOfferAndStudent_Id(Integer offerId, Integer studentId);
+    List<StudentOffer> findAllByOffer_IdOffer(Integer offerId);
 }
