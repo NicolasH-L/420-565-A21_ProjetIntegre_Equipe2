3f843301603d417f03098edfb05243f2115d358b
Anas <sinho919@hotmail.com>
2021-10-19 17:47:48 -0400

    EQ2-7 Backend: ajout methodes de test pour getAllStudentOffersByIdOffer dans le controllerTest

--- Projet_Integre_Equipe2/src/test/java/com/equipe2/projet_integre_equipe2/controller/StudentOfferControllerTest.java
+++ Projet_Integre_Equipe2/src/test/java/com/equipe2/projet_integre_equipe2/controller/StudentOfferControllerTest.java
@@ -5,6 +5,7 @@ import com.equipe2.projet_integre_equipe2.model.Offer;
 import com.equipe2.projet_integre_equipe2.model.Student;
 import com.equipe2.projet_integre_equipe2.model.StudentOffer;
 import com.equipe2.projet_integre_equipe2.service.StudentOfferService;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,6 +18,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -35,8 +38,10 @@ public class StudentOfferControllerTest {
     private StudentOffer studentOffer;
 
     private Document document;
+    private Document document2;
 
     private Student student;
+    private Student student2;
 
     private Offer offer;
 
@@ -67,12 +72,27 @@ public class StudentOfferControllerTest {
                 .data("test".getBytes(StandardCharsets.UTF_8))
                 .build();
 
+        document2 = Document.builder()
+                .documentName("CVExemple2")
+                .student(null)
+                .data("test2".getBytes(StandardCharsets.UTF_8))
+                .build();
+
         student = Student.studentBuilder()
                 .id(1)
                 .firstName("Toto")
                 .lastName("Tata")
                 .matricule("1234567")
                 .password("1234")
+                .isCvValid(true)
+                .build();
+
+        student2 = Student.studentBuilder()
+                .id(2)
+                .firstName("didi")
+                .lastName("coco")
+                .matricule("1234588")
+                .password("didi1*")
                 .isCvValid(true)
                 .build();
 
@@ -108,5 +128,35 @@ public class StudentOfferControllerTest {
 
         assertThat(result.getResponse().getStatus()).isEqualTo(HttpStatus.OK.value());
         assertThat(actualIsStudentOfferExist).isEqualTo(true);
+    }
+
+    @Test
+    public void testGetAllStudentOffersByOffer_IdOffer() throws Exception {
+        when(studentOfferService.getAllStudentOffersByOffer_IdOffer(offer.getIdOffer())).thenReturn(Optional.of(getListOfStudentOffersByIdOffer()));
+
+        MvcResult result = mockMvc.perform(get("/offers-list/get-all-studentOffersByIdOffer/" + offer.getIdOffer())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+
+        var actuals = new ObjectMapper().readValue(result.getResponse().getContentAsString(), new TypeReference<List<StudentOffer>>(){
+        });
+
+        assertThat(result.getResponse().getStatus()).isEqualTo(HttpStatus.OK.value());
+        assertThat(actuals.size()).isEqualTo(2);
+    }
+
+    private List<StudentOffer> getListOfStudentOffersByIdOffer() {
+        List<StudentOffer> studentOfferList = new ArrayList<>();
+        studentOfferList.add(StudentOffer.builder()
+                .offer(offer)
+                .document(document)
+                .student(student)
+                .build());
+        studentOfferList.add(StudentOffer.builder()
+                .offer(offer)
+                .document(document2)
+                .student(student2)
+                .build());
+        return  studentOfferList;
     }
 }
