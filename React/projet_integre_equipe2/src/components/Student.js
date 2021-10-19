8921cfdb92cfe4572f9114db29a8acb94b8d9531
IglooSyrup <darckuvina@gmail.com>
2021-10-19 15:18:31 -0400

    EQ2-28 - Frontend: ajout useState userStudent

--- React/projet_integre_equipe2/src/components/Student.js
+++ React/projet_integre_equipe2/src/components/Student.js
@@ -2,16 +2,18 @@ import React from 'react'
 import { useHistory } from 'react-router-dom'
 import StudentUploadCV from './StudentUploadCV'
 import StudentDocuments from './StudentDocuments'
-import { useEffect } from 'react'
+import { useEffect , useState} from 'react'
 import StudentNavbar from './StudentNavbar'
 
 const Student = () => {
     const history = useHistory()
     const historyState = history.location.state
-
+    const [userStudent, setUserStudent] = useState([])
+    
     return (
         <div>
             <StudentNavbar useStudent={historyState} />
+            
         </div>
     )
 }
