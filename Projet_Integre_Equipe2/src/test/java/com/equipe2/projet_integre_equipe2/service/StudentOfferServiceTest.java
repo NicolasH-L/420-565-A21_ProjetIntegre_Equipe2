c73036a8059a2c44e962628964900de0e415074e
Anas <sinho919@hotmail.com>
2021-10-19 17:07:37 -0400

    EQ2-7 Backend: ajout methodes de test pour getAllStudentOffersByIdOffer dans le serviceTest

--- Projet_Integre_Equipe2/src/test/java/com/equipe2/projet_integre_equipe2/service/StudentOfferServiceTest.java
+++ Projet_Integre_Equipe2/src/test/java/com/equipe2/projet_integre_equipe2/service/StudentOfferServiceTest.java
@@ -5,6 +5,7 @@ import com.equipe2.projet_integre_equipe2.model.Offer;
 import com.equipe2.projet_integre_equipe2.model.Student;
 import com.equipe2.projet_integre_equipe2.model.StudentOffer;
 import com.equipe2.projet_integre_equipe2.repository.StudentOfferRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +14,8 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -30,8 +33,10 @@ public class StudentOfferServiceTest {
     private Offer offer;
 
     private Document document;
+    private Document document2;
 
     private Student student;
+    private Student student2;
 
     private StudentOffer studentOffer;
 
@@ -62,12 +67,27 @@ public class StudentOfferServiceTest {
                 .data("test".getBytes(StandardCharsets.UTF_8))
                 .build();
 
+        document2 = Document.builder()
+                .documentName("CVExemple2")
+                .student(null)
+                .data("test2".getBytes(StandardCharsets.UTF_8))
+                .build();
+
         student = Student.studentBuilder()
                 .id(1)
                 .firstName("Toto")
                 .lastName("Tata")
                 .matricule("1234567")
                 .password("1234")
+                .isCvValid(true)
+                .build();
+
+        student2 = Student.studentBuilder()
+                .id(2)
+                .firstName("didi")
+                .lastName("coco")
+                .matricule("1234588")
+                .password("didi1*")
                 .isCvValid(true)
                 .build();
 
@@ -107,5 +127,39 @@ public class StudentOfferServiceTest {
         when(studentOfferRepository.existsStudentOfferByOffer_IdOfferAndStudent_Id(offer.getIdOffer(),student.getId())).thenReturn(true);
         Optional<Boolean> actualStudentAlreadyAppliedToOffer = studentOfferService.isStudentAppliedToOffer(offer.getIdOffer(), student.getId());
         assertThat(actualStudentAlreadyAppliedToOffer.get()).isTrue();
+    }
+
+    @Test
+    public void testGetAllStudentOffersByOffer_IdOffer() {
+        when(studentOfferRepository.saveAll(getListOfStudentOffersByIdOffer())).thenReturn(getListOfStudentOffersByIdOffer());
+        when(studentOfferRepository.findAllByOffer_IdOffer(offer.getIdOffer())).thenReturn(getListOfStudentOffersByIdOffer());
+        final Optional<List<StudentOffer>> expectedStudentOfferList = Optional.of(studentOfferRepository.saveAll(getListOfStudentOffersByIdOffer()));
+        final Optional<List<StudentOffer>> actualStudentOfferList = studentOfferService.getAllStudentOffersByOffer_IdOffer(offer.getIdOffer());
+        assertThat(actualStudentOfferList.get().size()).isEqualTo(expectedStudentOfferList.get().size());
+        assertThat(actualStudentOfferList.get().size()).isEqualTo(2);
+    }
+
+    @Test
+    public void testGetAllStudentOffersByOffer_IdOfferFails() {
+        when(studentOfferRepository.saveAll(getListOfStudentOffersByIdOffer())).thenReturn(getListOfStudentOffersByIdOffer());
+        when(studentOfferRepository.findAllByOffer_IdOffer(offer.getIdOffer())).thenReturn(null);
+        studentOfferRepository.saveAll(getListOfStudentOffersByIdOffer());
+        final Optional<List<StudentOffer>> studentOfferList = studentOfferService.getAllStudentOffersByOffer_IdOffer(offer.getIdOffer());
+        assertThat(studentOfferList).isEmpty();
+    }
+
+    private List<StudentOffer> getListOfStudentOffersByIdOffer() {
+        List<StudentOffer> studentOfferList = new ArrayList<>();
+        studentOfferList.add(StudentOffer.builder()
+                .offer(offer)
+                .document(document)
+                .student(student)
+                .build());
+        studentOfferList.add(StudentOffer.builder()
+                .offer(offer)
+                .document(document2)
+                .student(student2)
+                .build());
+        return  studentOfferList;
     }
 }
