b34de1259bda5224cc2c836518ff8967a60b80d2
Jonathan-Lorfils <jonathan.11911@gmail.com>
2021-10-19 19:55:18 -0400

    EQ2-30 Ajout du bouton permettant de changer de status et de sauvegarder le student

--- React/projet_integre_equipe2/src/components/Student.js
+++ React/projet_integre_equipe2/src/components/Student.js
@@ -8,16 +8,45 @@ const Student = () => {
     const history = useHistory()
     const historyState = history.location.state
     const [userStudent, setUserStudent] = useState([])
+    const [status, setStatus] = useState([])
 
     useEffect(() => {
         setUserStudent(historyState)
+        setStatus(historyState.currentStatus)
     }, [])
+
+    console.log(userStudent)
+
+    const addStudent = async (student) => {
+        const result = await fetch('http://localhost:8888/students/register',
+          {
+            method: 'POST',
+            headers: {
+              'Content-type': 'application/json'
+            },
+            body: JSON.stringify(student)
+          })
+        return await result.json()
+    }
 
     return (
         <div className="grad">
             <StudentNavbar useStudent={historyState} />
             <div className="d-flex justify-content-center">
                 <div className="container my-5">
+                    
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {status}
+                        </button>
+                        <div class="dropdown-menu">
+                            <button className="dropdown-item" onClick={() => { setUserStudent({...userStudent, currentStatus: "En Recherche"}) }}>En recherche</button>
+                            <button className="dropdown-item" onClick={() => { setUserStudent({...userStudent, currentStatus: "En attente"}) }} >En attente d'une entrevue</button>
+                            <button className="dropdown-item" onClick={() => { setUserStudent({...userStudent, currentStatus: "Stage trouvée"}) }}>Stage trouvée</button>
+                        </div>
+                    </div>
+                    <button onClick={() => { addStudent(userStudent) }}>Enregistrer</button>
+
                     <h1 className="text-center mb-5">Bienvenu {userStudent.firstName} {userStudent.lastName}</h1>
                     <StudentAppliedOffersList />
                 </div>
