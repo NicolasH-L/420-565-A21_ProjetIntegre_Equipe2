cd191aff8fdcdc7e81c6bd637e6dc9710300865f
Kevin Blaires <kablaires@gmail.com>
2021-10-19 14:05:49 -0400

    EQ2-23 - Frontend: Ajout d'une alerte pour savoir si le processus de signature est belle et bine lancé.

--- React/projet_integre_equipe2/src/components/Admin/AdminStudentAcceptedOffers.js
+++ React/projet_integre_equipe2/src/components/Admin/AdminStudentAcceptedOffers.js
@@ -34,47 +34,55 @@ const AdminStudentAcceptedOffers = () => {
                 body: JSON.stringify(offer)
             })
         const data = await res.json()
-        !data.isAccepted ? alert("Une erreur est survenue, veuillez réessayer plus tard!") : alert("Processus de signature commencé")
-
-        /*setAcceptedOffers(
+        
+        setAcceptedOffers(
             acceptedOffers.map(
-                (offer1) => offer1.idOffer === offer.idOffer ? {...offer1, valid: data.valid, state: data.state} : offer1
+                (offer1) => offer1.idApplication === offer.idApplication ? {...offer1, isAccepted: data.isAccepted} : offer1
             )
-        )*/
+        )
+
+        return data
     }
 
     return (
         <div>
             <div className="grad">
-            <AdminNavbar />
-            <h2 className="text-center">Offres de stage acceptés</h2>
-            <div className="p-5">
-                <table className="table table-hover bg-light shadow-lg">
-                    <thead>
-                        <tr>
-                            <th scope="col">Nom de l'étudiant</th>
-                            <th scope="col">Matricule</th>
-                            <th scope="col">Poste</th>
-                            <th scope="col"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {acceptedOffers.map((acceptedOffer) => (
-                            <tr key={acceptedOffer.idApplication}>
-                                <th>{acceptedOffer.student.firstName + " " + acceptedOffer.student.lastName}</th>
-                                <td>{acceptedOffer.student.matricule}</td>
-                                <td>{acceptedOffer.offer.jobTitle}$</td>
-                                <td className="w-25">
-                                    <button className="btn btn-primary mx-2" onClick={e => { e.preventDefault(); viewOffer(acceptedOffer) }}>Consulter</button>
-                                    <button className="btn btn-success mx-2" onClick={e => { e.preventDefault(); acceptOffer(acceptedOffer.offer) }}>Débuter signatures</button>
-                                    {/*<button className="btn btn-danger mx-2" onClick={e => { e.preventDefault(); declineOffer(acceptedOffer) }}>Refuser</button>*/}
-                                </td>
+                <AdminNavbar />
+                <h2 className="text-center">Offres de stage acceptés</h2>
+                <div className="p-5">
+                    <table className="table table-hover bg-light shadow-lg">
+                        <thead>
+                            <tr>
+                                <th scope="col">Nom de l'étudiant</th>
+                                <th scope="col">Matricule</th>
+                                <th scope="col">Poste</th>
+                                <th scope="col"></th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {acceptedOffers.map((acceptedOffer) => (
+                                <tr key={acceptedOffer.idApplication}>
+                                    <th>{acceptedOffer.student.firstName + " " + acceptedOffer.student.lastName}</th>
+                                    <td>{acceptedOffer.student.matricule}</td>
+                                    <td>{acceptedOffer.offer.jobTitle}$</td>
+                                    <td className="w-25">
+                                        <button className="btn btn-primary mx-2" onClick={e => { e.preventDefault(); viewOffer(acceptedOffer) }}>Consulter</button>
+                                        <button className="btn btn-success mx-2"
+                                            onClick={e => {
+                                                e.preventDefault();
+                                                acceptOffer(acceptedOffer)
+                                                    .then((data) => !data.isAccepted ? alert("Une erreur est survenue, veuillez réessayer plus tard!") : alert("Processus de signature commencé"))
+                                            }}>
+                                            Débuter signatures
+                                        </button>
+                                        {/*<button className="btn btn-danger mx-2" onClick={e => { e.preventDefault(); declineOffer(acceptedOffer) }}>Refuser</button>*/}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
         </div>
     )
 }
