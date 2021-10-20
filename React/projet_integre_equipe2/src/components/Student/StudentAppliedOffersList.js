5dccffbc47b067a4ff8d53c7d7a24a1631aeea7e
IglooSyrup <darckuvina@gmail.com>
2021-10-20 00:22:57 -0400

    EQ2-28 - Frontend: Ajout du tableau qui permet de voir les offres + fonctionnalité d'ajout d'une date

--- React/projet_integre_equipe2/src/components/Student/StudentAppliedOffersList.js
+++ React/projet_integre_equipe2/src/components/Student/StudentAppliedOffersList.js
@@ -5,43 +5,70 @@ import OfferModalView from '../OfferModalView'
 
 const StudentAppliedOffersList = () => {
 
-    const [offers, setOffers] = useState([])
     const [studentOffers, setStudentOffers] = useState([])
-    const [student, setStudent] = useState({})
     const history = useHistory()
     const historyState = history.location.state
     const timeElapsed = Date.now()
     const today = new Date(timeElapsed).toISOString().split('T')[0]
-    
+    const baseUrl = 'http://localhost:8888/offers-list'
+
     useEffect(() => {
         if (historyState === undefined)
             return
         const getStudentOffers = async () => {
             const studentOffersFromServer = await fetchStudentOffers()
             setStudentOffers(studentOffersFromServer)
-            console.log(studentOffersFromServer)
         }
-        setStudent(historyState)
         getStudentOffers()
     }, [])
 
-    const fetchOffers = async () => {
-        const res = await fetch('http://localhost:8888/offer/get-all-valid-offers')
+    const fetchStudentOffers = async () => {
+        const res = await fetch(`${baseUrl}/student-offers/student/${historyState.id}`)
         return await res.json()
     }
 
-    const fetchStudentOffers = async () =>{
-        const res = await fetch(`http://localhost:8888/offers-list/student-offers/student/${historyState.id}`)
-        return await res.json()
+    const updateStudentOfferDate = async (studentOffer) => {
+        const res = await fetch(`${baseUrl}/student-offer-add-date`,
+            {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(studentOffer)
+            })
+        const data = await res.json()
+        setStudentOffers(
+            studentOffers.map(
+                (studentOffer1) => studentOffer1.idStudentOffer === studentOffer.idStudentOffer ?
+                    { ...studentOffer1, interviewDate: data.interviewDate } : studentOffer1
+            )
+        )
+        alert("Ajout de la date d'entrevue avec succès!")
     }
 
-    const findFutureDate = () => {
-        let futureDate = new Date(timeElapsed)
-        futureDate.setDate(futureDate.getDate() + 220)
-        let futureDateFormat = futureDate.toISOString().split('T')[0]
-        return futureDateFormat
+    const chooseDate = (studentOffer) => {
+        let stringToDate = new Date(studentOffer.offer.startInternshipDate)
+        let last = new Date(stringToDate.getTime() - (1 * 24 * 60 * 60 * 1000))
+        let maxDate = new Date(last).toISOString().split('T')[0]
+        return <input type="date" min={today} max={maxDate} id={"interviewDate" + studentOffer.idStudentOffer} name="interviewDate" className="form-control text-center" onChange={(e) => setInterviewDate(e, studentOffer)} required></input>
     }
-    
+
+    const setInterviewDate = (e, studentOffer) => {
+        const indexOfStudentOffer = studentOffers.indexOf(studentOffer)
+        studentOffers[indexOfStudentOffer].interviewDate = e.target.value
+    }
+
+    const addStudentOfferInterviewDate = (studentOffer) => {
+        const index = studentOffers.indexOf(studentOffer)
+        if (index < 0) {
+            return
+        }
+        let interviewDate = studentOffers[index].interviewDate
+        if (interviewDate === null)
+            return alert("Erreur! veuillez choisir une date.")
+        updateStudentOfferDate(studentOffer)
+    }
+
     return (
         <div className="">
             <h2 className="text-center">Mes offres de stages</h2>
@@ -52,10 +79,8 @@ const StudentAppliedOffersList = () => {
                             <th scope="col" className="text-center">Entreprise</th>
                             <th scope="col" className="text-center">Poste</th>
                             <th scope="col" className="text-center">Salaire</th>
-                            <th scope="col" className="text-center">Date d'affichage</th>
-                            <th scope="col" className="text-center">Date limite d'affichage</th>
+                            <th scope="col" className="text-center">Date début stage</th>
                             <th scope="col" className="text-center">Date de l'entrevue</th>
-                            <th scope="col" className="text-center"></th>
                             <th scope="col" className="text-center"></th>
                         </tr>
                     </thead>
@@ -65,13 +90,16 @@ const StudentAppliedOffersList = () => {
                                 <th className="text-center">{studentOffer.offer.companyName}</th>
                                 <td className="text-center">{studentOffer.offer.jobTitle}</td>
                                 <td className="text-center">{studentOffer.offer.salary}$</td>
-                                <td className="text-center">{studentOffer.offer.displayDate}</td>
-                                <td className="text-center">{studentOffer.offer.deadlineDate}</td>
-                                <td className="text-center">{studentOffer.interviewDate === null ? "Aucune": studentOffer.interviewDate}</td>
-                                <td className="text-center">test</td>
-
-                                <td className="text-center"><OfferModalView newOffer={studentOffer.offer}/></td>
-                                <td className="text-center">test</td>
+                                <td className="text-center">{studentOffer.offer.startInternshipDate}</td>
+                                <td className="text-center">{studentOffer.interviewDate === null ? chooseDate(studentOffer) : studentOffer.interviewDate}</td>
+                                <td className="text-center">
+                                    <div className="row justify-content-center">
+                                        {studentOffer.interviewDate === null ?
+                                            <button type="button" className="btn btn-danger mr-5" onClick={() => addStudentOfferInterviewDate(studentOffer)}>Ajouter date</button> : ""
+                                        }
+                                        <OfferModalView newOffer={studentOffer.offer} displayMessageBoolean={false} />
+                                    </div>
+                                </td>
                             </tr>
                         ))}
                     </tbody>
@@ -79,7 +107,6 @@ const StudentAppliedOffersList = () => {
             </div>
         </div>
     )
-
 }
 
 export default StudentAppliedOffersList
