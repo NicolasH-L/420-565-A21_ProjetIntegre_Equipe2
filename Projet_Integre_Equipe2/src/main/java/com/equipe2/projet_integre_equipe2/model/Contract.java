73da416d249c5066ff1a502a06a755d67e2fb069
NicolasH-L <nhonglao@gmail.com>
2021-10-25 18:42:58 -0400

    EQ2-14 - Backend: Creation du model Contract

---
+++ Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/model/Contract.java
@@ -0,0 +1,34 @@
+package com.equipe2.projet_integre_equipe2.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import java.io.Serializable;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Contract implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Integer idContract;
+
+    @OneToOne
+    private Internship internship;
+
+    private String collegeResponsability;
+    private String enterpriseResponsability;
+    private String studentResponsability;
+    private String studentSignature;
+    private String monitorSignature;
+    private String adminSignature;
+    private String signatureDateStudent;
+    private String signatureDateMonitor;
+    private String signatureDateAdmin;
+
+}
