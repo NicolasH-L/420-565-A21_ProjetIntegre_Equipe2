c39bb27dbf925108c1b12c7e3e28569d7d1ba87e
Kevin Blaires <kablaires@gmail.com>
2021-10-29 21:08:14 -0400

    EQ2-164 - Frontend: Ajout d'un flitre pour voir que les internships de la session choisi.

--- React/projet_integre_equipe2/src/components/Admin/AdminInternshipList.js
+++ React/projet_integre_equipe2/src/components/Admin/AdminInternshipList.js
@@ -6,6 +6,8 @@ import { useState, useEffect } from 'react'
 const AdminInternshipList = () => {
     const [internships, setInternships] = useState([])
     const history = useHistory()
+    const historyState = history.location.state
+    const admin = historyState.admin
 
     useEffect(() => {
         const getInternships = async () => {
@@ -22,6 +24,10 @@ const AdminInternshipList = () => {
 
     const viewOffer = async (offer) => {
         history.push("/OfferView", offer)
+    }
+
+    const filterInterships = (internship) => {
+        return admin.actualSession === internship.session
     }
 
     return (
@@ -41,7 +47,9 @@ const AdminInternshipList = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            {internships.map((internship) => (
+                            {internships
+                            .filter(filterInterships)
+                            .map((internship) => (
                                 <tr key={internship.idApplication} className={internship.status == "Valide" ? 'table-success' : 'table-warning'}>
                                     <th>{internship.student.firstName + " " + internship.student.lastName}</th>
                                     <td>{internship.offer.jobTitle}</td>
