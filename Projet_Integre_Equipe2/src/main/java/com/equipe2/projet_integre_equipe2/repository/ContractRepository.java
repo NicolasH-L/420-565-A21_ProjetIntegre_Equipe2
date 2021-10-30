f31eb517984ff895497b7af44e90e65760d9ff5d
NicolasH-L <nhonglao@gmail.com>
2021-10-30 00:14:06 -0400

    EQ2-14 - Backend : Modification de la methode du repository de Contract

--- Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/repository/ContractRepository.java
+++ Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/repository/ContractRepository.java
@@ -4,10 +4,8 @@ import com.equipe2.projet_integre_equipe2.model.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface ContractRepository extends JpaRepository<Contract, Integer> {
 
-    List<Contract> findContractByInternship_Student_Id(Integer id);
+    Contract findContractByInternship_Student_Id(Integer id);
 }
