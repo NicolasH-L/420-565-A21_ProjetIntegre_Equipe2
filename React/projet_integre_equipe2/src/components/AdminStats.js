4c87be00ee32ccd07314689aa36dddcdef7abcdd
Anas <sinho919@hotmail.com>
2021-10-31 02:42:20 -0400

    EQ2-165 Frontend: ajout stats pour le gestionnaire

--- React/projet_integre_equipe2/src/components/AdminStats.js
+++ React/projet_integre_equipe2/src/components/AdminStats.js
@@ -1,6 +1,49 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 const AdminStats = () => {
+    const [students, setStudents] = useState([])
+    const [offers, setOffers] = useState([])
+    const [documents, setDocuments] = useState([])
+    const [validStudentsCount, setValidStudentsCount] = useState(0)
+
+    useEffect(() => {
+        const getAllStudents = async () => {
+            const studentsFromServer = await fetchAllStudents()
+            setStudents(studentsFromServer)
+        }
+        const getAllOffers = async () => {
+            const offersFromServer = await fetchAllOffers()
+            setOffers(offersFromServer)
+        }
+        const getAllValidStudents = async () => {
+            let validStudents = students.filter((student) => student.isCvValid === true)
+            setValidStudentsCount(validStudents.length)
+        }
+        const getAllInvalidDocuments = async () => {
+            const documentsFromServer = await fetchAllDocuments()
+            setDocuments(documentsFromServer.filter((document) => document.isValid === false))
+        }
+        getAllStudents()
+        getAllOffers()
+        getAllValidStudents()
+        getAllInvalidDocuments()
+    }, [offers.length])
+
+    const fetchAllStudents = async () => {
+        const res = await fetch("http://localhost:8888/students/get-all-students")
+        return await res.json()
+    }
+
+    const fetchAllOffers = async () => {
+        const res = await fetch("http://localhost:8888/offer/get-all-offers")
+        return await res.json()
+    }
+
+    const fetchAllDocuments = async () => {
+        const res = await fetch("http://localhost:8888/document/get-all-documents")
+        return await res.json()
+    }
 
     return (
         <div>
@@ -10,10 +53,10 @@ const AdminStats = () => {
                     <h2 className="text-center mb-3">Statistiques <i className="fas fa-chart-line text-success"></i></h2>
                     <div className="container-fluid">
                         <ul className="list-group">
-                            <a href="#" className="list-group-item list-group-item-action">Nombre d'étudiants inscrits: <span class="badge badge-primary badge-pill">5</span> </a>
-                            <a href="#" className="list-group-item list-group-item-action">Nombre de CV non-valides: <span class="badge badge-danger badge-pill">5</span> </a>
-                            <a href="#" className="list-group-item list-group-item-action">Nombre d'offres téléversées: <span class="badge badge-primary badge-pill">5</span> </a>
-                            <a href="#" className="list-group-item list-group-item-action">Nombre d'étudiants valides: <span class="badge badge-success badge-pill">5</span> </a>
+                            <a href="#" className="list-group-item list-group-item-action">Nombre d'étudiants inscrits: <span className="badge badge-primary badge-pill">{students.length}</span> </a>
+                            <a href="#" className="list-group-item list-group-item-action">Nombre de CV non-valides: <span className="badge badge-danger badge-pill">{documents.length}</span> </a>
+                            <a href="#" className="list-group-item list-group-item-action">Nombre d'offres téléversées: <span className="badge badge-primary badge-pill">{offers.length}</span> </a>
+                            <a href="#" className="list-group-item list-group-item-action">Nombre d'étudiants valides: <span className="badge badge-success badge-pill">{validStudentsCount}</span> </a>
                         </ul>
                     </div>
                 </div>
