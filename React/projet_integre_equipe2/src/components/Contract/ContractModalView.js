ec9b279f4d90191f088fc8673a18ce9a29ba9cd0
IglooSyrup <darckuvina@gmail.com>
2021-11-02 19:37:58 -0400

    EQ2-4 - modification en progres avant merge

--- React/projet_integre_equipe2/src/components/Contract/ContractModalView.js
+++ React/projet_integre_equipe2/src/components/Contract/ContractModalView.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
+
 const ContractModalView = () => {
+
+
     return (
         <div>
             
--- React/projet_integre_equipe2/src/components/Monitor/MonitorSignContract.js
+++ React/projet_integre_equipe2/src/components/Monitor/MonitorSignContract.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
-const MonitorSignContract = () => {
+const MonitorSignContract = ({monitorProp, contractProp}) => {
+    const monitor = monitorProp
+    const contract = contractProp
+
     return (
         <div>
             
