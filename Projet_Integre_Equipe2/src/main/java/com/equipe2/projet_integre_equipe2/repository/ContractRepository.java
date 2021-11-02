b61472b96d9bbd93c629a1196fcb608d775ff5ea
IglooSyrup <darckuvina@gmail.com>
2021-11-01 20:22:32 -0400

    EQ2-4 - Ajout findContractsByInternship_Offer_Monitor_Id

--- Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/repository/ContractRepository.java
+++ Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/repository/ContractRepository.java
@@ -4,8 +4,12 @@ import com.equipe2.projet_integre_equipe2.model.Contract;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ContractRepository extends JpaRepository<Contract, Integer> {
 
     Contract findContractByInternship_Student_Id(Integer id);
+
+    List<Contract> findContractsByInternship_Offer_Monitor_Id(Integer id);
 }
