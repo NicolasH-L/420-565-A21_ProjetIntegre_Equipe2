9a192be311dcfe64c6c21883a4fcd428a4760a3c
Kevin Blaires <kablaires@gmail.com>
2021-11-03 16:26:59 -0400

    EQ2-164 - Frontend: Ajout du bouton sessions au superviseur pour qu'il puissse avoir accès à tous les stages qui lui sont reliés.

--- React/projet_integre_equipe2/src/components/SessionsButton.js
+++ React/projet_integre_equipe2/src/components/SessionsButton.js
@@ -2,22 +2,30 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { useHistory, useLocation } from 'react-router-dom'
 
-
 const SessionsButton = () => {
     const [sessions, setSessions] = useState([])
     const history = useHistory()
     const historyState = history.location.state
-    const monitor = historyState.monitor
+    const [user, setUser] = useState()
     const location = useLocation()
 
     useEffect(() => {
+        setUser(historyState.monitor != undefined ? historyState.monitor 
+        : historyState.supervisor != undefined ? historyState.supervisor 
+        : historyState.admin != undefined ? historyState.admin : "")
         getSessions()
     }, [])
 
     const changeSession = (selectedSession) => {
-        monitor.actualSession = selectedSession
-        historyState.monitor = monitor
-        history.replace(location.pathname, historyState)
+        user.actualSession = selectedSession
+        if (historyState.monitor != undefined){
+            historyState.monitor = user
+        } else if(historyState.supervisor != undefined){
+            historyState.supervisor = user
+        } else if(historyState.admin != undefined){
+            historyState.admin = user
+        } 
+        history.push(location.pathname, historyState)
     }
 
     const getSessions = async () => {
@@ -48,7 +56,7 @@ const SessionsButton = () => {
                     <button
                         type="button"
                         key={session.idSession}
-                        className={`dropdown-item ${session.session === monitor.actualSession ? 'active' : ''}`}
+                        className={`dropdown-item ${session.session === user.actualSession ? 'active' : ''}`}
                         onClick={(e) => {e.preventDefault(); changeSession(session.session)}}>
                         {sessionValueToFrench(session.session)}
                     </button>
