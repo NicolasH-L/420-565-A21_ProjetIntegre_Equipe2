3b25d7a6500f2f003e788ddc0086540db4924932
Anas <sinho919@hotmail.com>
2021-11-09 12:09:19 -0500

    amélioration landing page ajout de css et d'image pour améliorer le visuel

--- anasIonic/src/pages/Home.tsx
+++ anasIonic/src/pages/Home.tsx
@@ -7,16 +7,29 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Home</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
+        {/* <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <ExploreContainer /> */}
+        <div className="bgimg">
+          <div className="topleft">
+            <p style={{ fontSize: "40px" }}> &#9728;</p>
+          </div>
+          <div className="middle">
+            <h1>Welcome</h1>
+            <hr />
+            <p>Stage Finder &#9733;</p>
+          </div>
+          <div className="bottomleft">
+            <p style={{ fontSize: "16px" }}>&copy; Copyright 2021 - 420-565-AL</p>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
