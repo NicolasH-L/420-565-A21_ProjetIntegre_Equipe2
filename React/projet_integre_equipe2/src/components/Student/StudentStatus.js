5769c4808485a7a34027dbc8fea2625b1aaf4512
Kevin Blaires <kablaires@gmail.com>
2021-11-09 18:21:10 -0500

    EQ2-167 - Frontend: Création du nouveau UX du profil de l'étudiant.

---
+++ React/projet_integre_equipe2/src/components/Student/StudentStatus.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useHistory } from 'react-router'
+import StudentNavbar from '../StudentNavbar';
+
+const StudentStatus = () => {
+    const history = useHistory();
+    const historyState = history.location.state
+    const student = historyState.student
+
+    return (
+        <div className="grad">
+            <StudentNavbar />
+            <h2 className="text-center">Bienvenue {student.firstName + " " + student.lastName}</h2>
+            <div className="d-flex justify-content-center">
+                <div class="card text-center shadow w-50 my-5">
+                    <div class="card-header font-weight-bold">
+                        Votre statut actuel: <p className="text-success">{student.currentStatus}</p>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Avez-vous une date d'entevue ou trouvé un stage?</h5>
+                        <p class="card-text">Cliquez sur l'option qui vous correspond</p>
+                        <a href="#" class="btn btn-primary mx-2">
+                            <i className="fas fa-hourglass-start mr-2"></i> En recherche
+                        </a>
+                        <a href="#" class="btn btn-primary mx-2">
+                            <i className="fas fa-calendar-alt mr-2"></i> Date d'entrevue
+                        </a>
+                        <a href="#" class="btn btn-primary mx-2">
+                            <i className="fas fa-handshake mr-2"></i> Stage trouvé
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default StudentStatus
