37f5b7eb4d4775ae2c2036fefa3dedf0b7ff4633
Kevin Blaires <kablaires@gmail.com>
2021-11-10 17:55:35 -0500

    EQ2-167 - Frontend: Ajout d'un badge pour l'affiche du statut actuel

--- React/projet_integre_equipe2/src/components/Student/StudentStatus.js
+++ React/projet_integre_equipe2/src/components/Student/StudentStatus.js
@@ -24,20 +24,22 @@ const StudentStatus = () => {
 
     return (
         <div className="grad">
-            <StudentNavbar useStudent={student}/>
+            <StudentNavbar useStudent={student} />
             <h2 className="text-center">Bienvenue {student.firstName + " " + student.lastName}</h2>
             <div className="d-flex justify-content-center">
                 <div className="card text-center shadow w-50 my-5">
                     <div className="card-header font-weight-bold">
-                        Votre statut actuel: <p className="text-success">{student.currentStatus}</p>
+                        <h5>
+                            Votre statut actuel: <span className="badge badge-success">{student.currentStatus}</span>
+                        </h5>
                     </div>
                     <div className="card-body">
                         <h5 className="card-title">Avez-vous une date d'entevue ou trouvé un stage?</h5>
                         <p className="card-text">Cliquez sur l'option qui vous correspond!</p>
                         <div className="d-flex justify-content-center">
-                            <StudentSearchingStatus onAddStudent={addStudent}/>
+                            <StudentSearchingStatus onAddStudent={addStudent} />
                             <StudentInterviewDateStatus />
-                            <StudentInternshipFindedStatus onAddStudent={addStudent}/>
+                            <StudentInternshipFindedStatus onAddStudent={addStudent} />
                         </div>
                     </div>
                 </div>
