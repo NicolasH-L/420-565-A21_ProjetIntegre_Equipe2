fb74b7a6a889664ea84480d31c44245c5dcb4763
IglooSyrup <darckuvina@gmail.com>
2021-11-17 13:07:56 -0500

    EQ2-9 - Frontend: les choix de réponses pour chaque capacité

--- React/projet_integre_equipe2/src/components/Evaluation/RatingChoices.js
+++ React/projet_integre_equipe2/src/components/Evaluation/RatingChoices.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 
-const RatingChoices = ({ onChangeMethod, name }) => {
+const RatingChoices = ({ onChangeMethod, newName }) => {
 
-
-    const getChoices = (name) => {
+    const getChoices = () => {
         return (
-            <select defaultValue="default" className="form-control" name={name} onChange={onChangeMethod} id="">
+            <select defaultValue="default" className="form-control" name={newName} onChange={onChangeMethod}>
                 <option className="text-center" value="default">Veuillez choisir une valeur</option>
                 <option className="text-center" value="Totalement en accord">Totalement en accord</option>
                 <option className="text-center" value="Plutôt en accord">Plutôt en accord</option>
