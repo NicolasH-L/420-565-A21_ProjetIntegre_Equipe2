4f45e9c886c4ec5e30fc36d408c099257525ed3f
IglooSyrup <darckuvina@gmail.com>
2021-11-17 13:05:43 -0500

    EQ2-9 - Frontend: vue modal du formulaire d'evaluation

---
+++ React/projet_integre_equipe2/src/components/Evaluation/EvaluationModalView.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import EvaluationForm from './EvaluationForm'
+
+const EvaluationModalView = ({ contractProp, isModeEdit }) => {
+    const [contract, setContract] = useState(null)
+    const isEdit = isModeEdit
+    useEffect(() => {
+        setContract(contractProp)
+    }, [])
+
+    return (
+        <div>
+            {contract !== null
+                ?
+                <>
+                    <button className="btn btn-primary mr-5" data-toggle="modal" data-target={"#internship" + contract.internship.idInternship}>Consulter</button>
+                    <div className="modal fade" id={"internship" + contract.internship.idInternship} tabIndex="-1" role="dialog">
+                        <div className="modal-dialog modal-lg" role="document">
+                            <div className="modal-content">
+                                <EvaluationForm contractProp={contract} isModeEdit={isEdit}/>
+                                <div className="modal-footer mt-4">
+                                    <button type="button" className="btn btn-danger" data-dismiss="modal">Fermer</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </>
+                : ""}
+        </div>
+    )
+}
+
+export default EvaluationModalView
