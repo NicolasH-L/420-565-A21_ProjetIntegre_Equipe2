676eb48a8500ff69ee86e0b5fa309712396ab2e3
IglooSyrup <darckuvina@gmail.com>
2021-11-17 13:10:26 -0500

    EQ2-9 - Frontend: component qui permet au moniteur de voir les evaluations et evaluer un stagiaire

--- React/projet_integre_equipe2/src/components/Monitor/MonitorEvaluateStudent.js
+++ React/projet_integre_equipe2/src/components/Monitor/MonitorEvaluateStudent.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import MonitorNavbar from '../MonitorNavbar'
-import StudentEvaluationModalView from '../Evaluation/StudentEvaluationModalView'
+import EvaluationModalView from '../Evaluation/EvaluationModalView'
 
 const MonitorEvaluateStudent = () => {
     const history = useHistory()
@@ -13,33 +13,40 @@ const MonitorEvaluateStudent = () => {
 
     useEffect(() => {
         const getAllInternships = async () => {
-            const contractsFromServer = await fetchInternships()
-            setContracts(contractsFromServer)
+            const contractsFromServer = await fetchContracts()
+            setContracts(contractsFromServer.filter(hasSupervisor))
         }
         getAllInternships()
     }, [])
 
-    const fetchInternships = async () => {
+    const fetchContracts = async () => {
         const res = await fetch(`http://localhost:8888/contract/get-all-by-monitor/${monitor.id}/status/${status}`)
         const data = await res.json()
         return data
     }
 
-    const bySession = (internship) => {
-        return monitor.actualSession === internship.session
+    // TODO update the isEvaluatedByMonitor with POST method
+    
+
+    const hasSupervisor = (contract) => {
+        return contract.internship.supervisor !== null && contract.internship.supervisor !== undefined
+    }
+
+    const bySession = (contract) => {
+        return monitor.actualSession === contract.session
     }
 
     const isDisplayEvaluations = () => {
         return (contracts !== undefined || contracts !== null) && contracts.length > 0
     }
 
-    const displayEmptyErrorMessage = () =>{
+    const displayEmptyErrorMessage = () => {
         return (
             <div className="container">
-            <div className="d-flex justify-content-center">
-                <h2 className="text-dark">Vous n'avez pas de stagiaire à évaluer</h2>
+                <div className="d-flex justify-content-center">
+                    <h2 className="text-dark">Vous n'avez pas de stagiaire à évaluer</h2>
+                </div>
             </div>
-        </div>
         )
     }
 
@@ -56,7 +63,7 @@ const MonitorEvaluateStudent = () => {
                                     <th scope="col">Position</th>
                                     <th scope="col">Début du stage</th>
                                     <th scope="col">Nom de l'étudiant</th>
-                                    <th scope="col">Progrès</th>
+                                    <th scope="col">Statut</th>
                                     <th></th>
                                 </tr>
                             </thead>
@@ -64,13 +71,13 @@ const MonitorEvaluateStudent = () => {
                                 {contracts
                                     .filter(bySession)
                                     .map((contract) => (
-                                        <tr key={contract.internship.idInternship} className="text-center">
+                                        <tr key={contract.idContract} className="text-center">
                                             <td>{contract.internship.offer.jobTitle}</td>
                                             <td>{contract.internship.offer.startInternshipDate}</td>
                                             <td>{contract.internship.student.firstName + " " + contract.internship.student.lastName}</td>
-                                            <td></td>
-                                            <td className="w-25">
-                                                <StudentEvaluationModalView contractProp={contract} isModeEdit={true}/>
+                                            <td>{contract.isEvaluatedByMonitor === true ? "Evaluation complété" : "En attente d'évaluation"}</td>
+                                            <td>
+                                                <EvaluationModalView contractProp={contract} isModeEdit={true} />
                                             </td>
                                         </tr>
                                     ))}
