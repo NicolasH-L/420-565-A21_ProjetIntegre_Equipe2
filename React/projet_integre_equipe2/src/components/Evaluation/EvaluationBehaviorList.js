d295671188bc1addeafaf66656036653fdff53fc
IglooSyrup <darckuvina@gmail.com>
2021-11-18 14:21:28 -0500

    EQ2-9 - Frontend: Ajout de la validation

---
+++ React/projet_integre_equipe2/src/components/Evaluation/EvaluationBehaviorList.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useEffect } from 'react'
+import Evaluations from '../Constants/Evaluations'
+import EvaluationBehavior from './EvaluationBehavior'
+
+const EvaluationBehaviorList = ({ behaviors, setBehaviors, submitState }) => {
+
+    useEffect(() => {
+        setBehaviors(Evaluations.behaviors())
+    }, [])
+
+    const displayBehaviors = () => {
+        return (
+            <>
+                {behaviors.map(behavior => (
+                    <div key={behavior.header}>
+                        <EvaluationBehavior newBehavior={behavior} submitState={submitState}/>
+                    </div>
+                ))}
+            </>
+        )
+    }
+
+    return (
+        <div className="form-group">
+            {behaviors !== null ?
+                displayBehaviors()
+                : ""}
+        </div>
+    )
+}
+
+export default EvaluationBehaviorList
