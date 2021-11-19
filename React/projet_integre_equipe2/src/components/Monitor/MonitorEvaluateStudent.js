5c18763be454d43564c7bf5e7e64279909578eb1
IglooSyrup <darckuvina@gmail.com>
2021-11-19 05:24:20 -0500

    EQ2-9 - Frontend: Ajout du fetch + fonctionnalités pour l'affichage de l'évaluation + téléchargement

--- React/projet_integre_equipe2/src/components/Monitor/MonitorEvaluateStudent.js
+++ React/projet_integre_equipe2/src/components/Monitor/MonitorEvaluateStudent.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import MonitorNavbar from '../MonitorNavbar'
 import EvaluationModalView from '../Evaluation/EvaluationModalView'
+import EvaluationPdfModalView from '../Evaluation/EvaluationPdfModalView'
 
 const MonitorEvaluateStudent = () => {
     const history = useHistory()
@@ -10,23 +11,30 @@ const MonitorEvaluateStudent = () => {
     const monitor = historyState.monitor
     const status = "Completed"
     const [contracts, setContracts] = useState([])
+    const [evaluations, setEvaluations] = useState([])
 
     useEffect(() => {
         const getAllInternships = async () => {
             const contractsFromServer = await fetchContracts()
             setContracts(contractsFromServer.filter(hasSupervisor).filter(bySession))
         }
+        const getAllEvaluations = async () => {
+            const evaluationsFromServer = await fetchEvaluations()
+            setEvaluations(evaluationsFromServer.filter(bySession))
+        }
         getAllInternships()
+        getAllEvaluations()
     }, [monitor.actualSession])
 
     const fetchContracts = async () => {
         const res = await fetch(`http://localhost:8888/contract/get-all-by-monitor/${monitor.id}/status/${status}`)
-        const data = await res.json()
-        return data
+        return await res.json()
     }
 
-    // TODO fetch all Evaluations if the evaluation has the same id as contract, do not show the contract
-    // show the evaluation pdf instead
+    const fetchEvaluations = async () => {
+        const res = await fetch(`http://localhost:8888/evaluation/get-all-by-monitor/${monitor.id}`)
+        return await res.json()
+    }
 
     const hasSupervisor = (contract) => {
         return contract.internship.supervisor !== null && contract.internship.supervisor !== undefined
@@ -48,6 +56,25 @@ const MonitorEvaluateStudent = () => {
                 </div>
             </div>
         )
+    }
+
+    const isInternAlreadyEvaluated = (contract) => {
+        let evaluationIndex = undefined
+        if (evaluations.length > 0)
+            for (let i = 0; i < evaluations.length; i++) {
+                if (evaluations[i].contract.idContract === contract.idContract) {
+                    evaluationIndex = i
+                    break
+                }
+            }
+        return evaluationIndex
+    }
+
+    const displayEvaluationButtons = (contract) => {
+        let index = isInternAlreadyEvaluated(contract)
+        return (index !== undefined ?
+            <EvaluationPdfModalView evaluation={evaluations[index]} />
+            : <EvaluationModalView contractProp={contract} />)
     }
 
     return (
@@ -75,11 +102,11 @@ const MonitorEvaluateStudent = () => {
                                             <td>{contract.internship.offer.jobTitle}</td>
                                             <td>{contract.internship.offer.startInternshipDate}</td>
                                             <td>{contract.internship.student.firstName + " " + contract.internship.student.lastName}</td>
-                                            <td>{contract.isEvaluatedByMonitor === true ? "Evaluation complété" : "En attente d'évaluation"}</td>
+                                            <td className={isInternAlreadyEvaluated(contract) !== undefined ? "table-success" : "table-warning"}>{isInternAlreadyEvaluated(contract) === true ? "Evaluation complété" : "En attente d'évaluation"}</td>
                                             <td>
-                                                {/* TODO fetch all Evaluations if the evaluation has the same id as contract, do not show the contract */}
-                                                {/*  show the evaluation pdf instead */}
-                                                <EvaluationModalView contractProp={contract} />
+                                                {
+                                                    displayEvaluationButtons(contract)
+                                                }
                                             </td>
                                         </tr>
                                     ))}
