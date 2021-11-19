069aea3b448e52658526ef296e7f23fe46c0354d
IglooSyrup <darckuvina@gmail.com>
2021-11-19 05:10:59 -0500

    EQ2-9 - Backend; Ajout EvaluationRepository

--- Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/repository/EvaluationRepository.java
+++ Projet_Integre_Equipe2/src/main/java/com/equipe2/projet_integre_equipe2/repository/EvaluationRepository.java
@@ -4,7 +4,12 @@ import com.equipe2.projet_integre_equipe2.model.Evaluation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EvaluationRepository extends JpaRepository<Evaluation, Integer> {
 
+    List<Evaluation> findEvaluationsByContract_Internship_Offer_Monitor_Id(Integer monitorId);
+
+    Evaluation findEvaluationByContract_Internship_Supervisor_IdAndContract_Internship_Student_Id(Integer supervisorId, Integer studentId);
 }
