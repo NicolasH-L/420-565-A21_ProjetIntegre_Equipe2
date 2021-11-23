a1e162879edb0a706f2a20a5531a3b45453c13fe
IglooSyrup <darckuvina@gmail.com>
2021-11-23 00:59:58 -0500

    EQ2-205 - Frontend: Ajout de l'affichage vide + modification du fetch de contrat par studentId et actualSession

--- React/projet_integre_equipe2/src/components/Student/StudentSignContract.js
+++ React/projet_integre_equipe2/src/components/Student/StudentSignContract.js
@@ -19,44 +19,64 @@ const StudentSignContract = () => {
         if (historyState === undefined)
             return
         const getInternship = async () => {
-            const internshipFromServer = await fetchInternship(student.id)
-            setInternship(internshipFromServer)
+            await fetchInternship()
+                .then((data) => data !== null ? setInternship(data) : setInternship(null))
         }
+
         const getContract = async () => {
-            const contractFromServer = await fetchContract(student.id)
-            setContract(contractFromServer)
+            await fetchContract()
+                .then((data) => data !== null ? setContract(data) : setContract(null))
         }
         getInternship()
         getContract()
-    }, [])
+    }, [student.actualSession])
 
-    const fetchInternship = async (idStudent) => {
-        const res = await fetch(`${baseUrl}/internship/get-internship/${idStudent}`)
+    const fetchInternship = async () => {
+        const res = await fetch(`${baseUrl}/internship/get-internship/${student.id}/session/${student.actualSession}`)
+        
+        if (res.status > 200)
+            return null
+
         return await res.json()
     }
 
-    const fetchContract = async (idStudent) => {
-        const res = await fetch(`${baseUrl}/contract/get-contract/${idStudent}`)
+    const fetchContract = async () => {
+        const res = await fetch(`${baseUrl}/contract/get-contract/${student.id}/session/${student.actualSession}`)
+        
+        if (res.status > 200)
+            return null
+
         return await res.json()
+    }
+
+    const displayEmptyErrorMessage = () => {
+        return (
+            <div className="container">
+                <div className="d-flex justify-content-center py-5">
+                    <h2 className="text-dark">Vous n'avez pas de contrats</h2>
+                </div>
+            </div>
+        )
     }
 
     return (
         <div className="grad">
             <StudentNavbar useStudent={student} />
-            {internship && contract && (
+            {internship !== null && contract !== null ? (
                 <div className="d-flex justify-content-center my-5 py-2">
                     <div className="jumbotron jumbotron-fluid bg-light rounded w-50 shadow reactivescreen">
-                        {(contract.internship.status === contractCompletedStatus) ? 
-                        <div className="d-flex justify-content-center mb-4">
-                            <DownloadContract contract={contract}></DownloadContract>
-                        </div>
-                        : ""}
+                        {(contract.internship.status === contractCompletedStatus) ?
+                            <div className="d-flex justify-content-center mb-4">
+                                <DownloadContract contract={contract}></DownloadContract>
+                            </div>
+                            : ""}
                         <Contract passwordUser={student.password}
                             currentStatus={Signature.getStudentSignatureStatus()} contractProp={contract}
                             signature={contract.studentSignature} />
                     </div>
                 </div>
-            )}
+            )
+                : displayEmptyErrorMessage()}
         </div>
     )
 }
