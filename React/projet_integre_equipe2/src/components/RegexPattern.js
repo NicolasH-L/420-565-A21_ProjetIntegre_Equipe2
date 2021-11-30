e317a02c8ca739f4df7eb2e32cfb1766fc2450b6
IglooSyrup <darckuvina@gmail.com>
2021-11-30 15:53:34 -0500

    Frontend: Optimisation du pattern general pour que ca consomme moins de memoire

--- React/projet_integre_equipe2/src/components/RegexPattern.js
+++ React/projet_integre_equipe2/src/components/RegexPattern.js
@@ -33,6 +33,6 @@ const patternEmail = /^([a-zA-Z0-9]+[\._:$!%\-+]{0,1}([a-zA-Z0-9])+)+@(([a-zA-Z0
 const patternName = /^([a-zA-ZéÉèÈïÏêÊ])(([a-zA-ZéÉèÈïÏêÊ]*|\-)[a-zA-ZéÉèÈïÏêÊ])*[a-zA-ZéÉèÈïÏêÊ]*$/
 const patternPassword = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/
 const patternMatricule = /^[0-9]{7}$/
-const patternGeneral = /^[^.]+([\s]{0,1}[^.]+)+$/
+const patternGeneral = /^.+([\s]{0,1}.+)+$/
 const patternNumber = /^([0-9]+\.?[0-9]*|\.[0-9]+)$/
 const patternTelephone = /^[0-9]{3}[-][0-9]{3}[-][0-9]{4}$/
