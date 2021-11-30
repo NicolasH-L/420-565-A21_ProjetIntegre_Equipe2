d7deddcb53f4253bb7f4d9e66d150008c40b9c35
NicolasH-L <nhonglao@gmail.com>
2021-11-29 22:32:49 -0500

    EQ2-206 - Front-end: Ajustement des importations

--- React/projet_integre_equipe2/src/components/Admin/AdminNotifications.js
+++ React/projet_integre_equipe2/src/components/Admin/AdminNotifications.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Notification } from '../Notification/Notification'
 
 const AdminNotifications = ({adminState}) => {
