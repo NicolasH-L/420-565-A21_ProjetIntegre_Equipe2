ab9e9e7d99af1a3a8285e356e8065092564aa52f
IglooSyrup <darckuvina@gmail.com>
2021-11-30 14:56:10 -0500

    Frontend: optimisation des espaces + modification du pattern general

--- React/projet_integre_equipe2/src/components/RegexPattern.js
+++ React/projet_integre_equipe2/src/components/RegexPattern.js
@@ -1,43 +1,38 @@
-export class RegexPattern{
-    
-    static getPatternEmail(){
-        return patternEmail
-    } 
+export class RegexPattern {
 
-    static getPatternName(){
+    static getPatternEmail() {
+        return patternEmail
+    }
+
+    static getPatternName() {
         return patternName
     }
 
-    static getPatternCompany(){
-        return patternCompany
-    }
-
-    static getPatternPassword(){
+    static getPatternPassword() {
         return patternPassword
     }
 
-    static getPatternMatricule(){
+    static getPatternMatricule() {
         return patternMatricule
     }
 
-    static getPatternGeneral(){
+    static getPatternGeneral() {
         return patternGeneral
     }
 
-    static getPatternNumber(){
+    static getPatternNumber() {
         return patternNumber
     }
 
-    static getPatternTelephone(){
+    static getPatternTelephone() {
         return patternTelephone
     }
 }
 
- const patternEmail = /^([a-zA-Z0-9]+[\._:$!%\-+]{0,1}([a-zA-Z0-9])+)+@(([a-zA-Z0-9])+[\.\-]{0,1}([a-zA-Z0-9])+)+\.[a-zA-Z0-9]{2,4}$/
- const patternName = /^([a-zA-ZéÉèÈïÏêÊ])(([a-zA-ZéÉèÈïÏêÊ]*|\-)[a-zA-ZéÉèÈïÏêÊ])*[a-zA-ZéÉèÈïÏêÊ]*$/
- const patternCompany = /^[^ ]+([ ]{0,1}[^ ]+)+$/
- const patternPassword = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/
- const patternMatricule = /^[0-9]{7}$/
- const patternGeneral = /^[^ ]+([ ]{0,1}[^ ]+)+$/
- const patternNumber = /^([0-9]+\.?[0-9]*|\.[0-9]+)$/
- const patternTelephone = /^[0-9]{3}[-][0-9]{3}[-][0-9]{4}$/
+const patternEmail = /^([a-zA-Z0-9]+[\._:$!%\-+]{0,1}([a-zA-Z0-9])+)+@(([a-zA-Z0-9])+[\.\-]{0,1}([a-zA-Z0-9])+)+\.[a-zA-Z0-9]{2,4}$/
+const patternName = /^([a-zA-ZéÉèÈïÏêÊ])(([a-zA-ZéÉèÈïÏêÊ]*|\-)[a-zA-ZéÉèÈïÏêÊ])*[a-zA-ZéÉèÈïÏêÊ]*$/
+const patternPassword = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/
+const patternMatricule = /^[0-9]{7}$/
+const patternGeneral = /^[^.]+([\s]{0,1}[^.]+)+$/
+const patternNumber = /^([0-9]+\.?[0-9]*|\.[0-9]+)$/
+const patternTelephone = /^[0-9]{3}[-][0-9]{3}[-][0-9]{4}$/
